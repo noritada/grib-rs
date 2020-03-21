c20cb62ad693ec4c174c10d617e18a14b7d4a3a0
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2020-03-21 12:08:47 +0900

    Cleanup: automatic reformat using `cargo fmt`
    This change ceases warnings from the `deprecated` lint concerning usage
    of `try!` macros.

--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,7 @@ use std::result::Result;
 
 pub fn unpack_sect0(f: &mut File) -> Result<usize, String> {
     let mut buf = [0; 16];
-    try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+    f.read_exact(&mut buf[..]).map_err(clarify_err)?;
 
     if buf[0] != b'G' || buf[1] != b'R' || buf[2] != b'I' || buf[3] != b'B' {
         return Err("not a GRIB file".to_string());
@@ -26,7 +26,7 @@ pub fn unpack_sect0(f: &mut File) -> Result<usize, String> {
 
 pub fn unpack_sect1_body(f: &mut File, sect_size: usize) -> Result<(), String> {
     let mut buf = [0; 16]; // octet 6-21
-    try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+    f.read_exact(&mut buf[..]).map_err(clarify_err)?;
 
     let master_table_version = buf[4];
     println!(
@@ -49,7 +49,7 @@ pub fn unpack_sect1_body(f: &mut File, sect_size: usize) -> Result<(), String> {
         // placeholder
         println!("reading extra {} bytes", len_extra);
         let mut buf = vec![0; len_extra];
-        try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+        f.read_exact(&mut buf[..]).map_err(clarify_err)?;
     }
 
     Ok(())
@@ -61,7 +61,7 @@ pub fn unpack_sect2_body(f: &mut File, sect_size: usize) -> Result<(), String> {
         // placeholder
         println!("reading extra {} bytes", len_extra);
         let mut buf = vec![0; len_extra];
-        try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+        f.read_exact(&mut buf[..]).map_err(clarify_err)?;
     }
 
     Ok(())
@@ -69,7 +69,7 @@ pub fn unpack_sect2_body(f: &mut File, sect_size: usize) -> Result<(), String> {
 
 pub fn unpack_sect3_body(f: &mut File, sect_size: usize) -> Result<(), String> {
     let mut buf = [0; 9]; // octet 6-14
-    try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+    f.read_exact(&mut buf[..]).map_err(clarify_err)?;
 
     // octet 7-10
     let npoints = concat_bytes_as_u32(&mut buf, 1, 4);
@@ -84,7 +84,7 @@ pub fn unpack_sect3_body(f: &mut File, sect_size: usize) -> Result<(), String> {
         // placeholder
         println!("reading extra {} bytes", len_extra);
         let mut buf = vec![0; len_extra];
-        try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+        f.read_exact(&mut buf[..]).map_err(clarify_err)?;
     }
 
     Ok(())
@@ -92,7 +92,7 @@ pub fn unpack_sect3_body(f: &mut File, sect_size: usize) -> Result<(), String> {
 
 pub fn unpack_sect4_body(f: &mut File, sect_size: usize) -> Result<(), String> {
     let mut buf = [0; 4]; // octet 6-9
-    try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+    f.read_exact(&mut buf[..]).map_err(clarify_err)?;
 
     // octet 6-7
     let ncoordinates = concat_bytes_as_u16(&mut buf, 0, 2);
@@ -110,7 +110,7 @@ pub fn unpack_sect4_body(f: &mut File, sect_size: usize) -> Result<(), String> {
         // placeholder
         println!("reading extra {} bytes", len_extra);
         let mut buf = vec![0; len_extra];
-        try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+        f.read_exact(&mut buf[..]).map_err(clarify_err)?;
     }
 
     Ok(())
@@ -118,7 +118,7 @@ pub fn unpack_sect4_body(f: &mut File, sect_size: usize) -> Result<(), String> {
 
 pub fn unpack_sect5_body(f: &mut File, sect_size: usize) -> Result<(), String> {
     let mut buf = [0; 6]; // octet 6-11
-    try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+    f.read_exact(&mut buf[..]).map_err(clarify_err)?;
 
     // octet 6-9
     let npoints7 = concat_bytes_as_u32(&mut buf, 0, 4);
@@ -139,7 +139,7 @@ pub fn unpack_sect5_body(f: &mut File, sect_size: usize) -> Result<(), String> {
         // placeholder
         println!("reading extra {} bytes", len_extra);
         let mut buf = vec![0; len_extra];
-        try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+        f.read_exact(&mut buf[..]).map_err(clarify_err)?;
     }
 
     Ok(())
@@ -147,7 +147,7 @@ pub fn unpack_sect5_body(f: &mut File, sect_size: usize) -> Result<(), String> {
 
 pub fn unpack_sect6_body(f: &mut File, sect_size: usize) -> Result<(), String> {
     let mut buf = [0; 1]; // octet 6
-    try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+    f.read_exact(&mut buf[..]).map_err(clarify_err)?;
 
     let bitmap_indicator = buf[0];
     println!("bit-map indicator: {}", bitmap_indicator);
@@ -157,7 +157,7 @@ pub fn unpack_sect6_body(f: &mut File, sect_size: usize) -> Result<(), String> {
         // placeholder
         println!("reading extra {} bytes", len_extra);
         let mut buf = vec![0; len_extra];
-        try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+        f.read_exact(&mut buf[..]).map_err(clarify_err)?;
     }
 
     Ok(())
@@ -169,7 +169,7 @@ pub fn unpack_sect7_body(f: &mut File, sect_size: usize) -> Result<(), String> {
         // placeholder
         println!("reading extra {} bytes", len_extra);
         let mut buf = vec![0; len_extra]; // octet 6-21
-        try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+        f.read_exact(&mut buf[..]).map_err(clarify_err)?;
     }
 
     Ok(())
@@ -177,7 +177,7 @@ pub fn unpack_sect7_body(f: &mut File, sect_size: usize) -> Result<(), String> {
 
 pub fn unpack_sect8(f: &mut File) -> Result<(), String> {
     let mut buf = [0; 4];
-    try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+    f.read_exact(&mut buf[..]).map_err(clarify_err)?;
 
     if buf[0] != b'7' || buf[1] != b'7' || buf[2] != b'7' || buf[3] != b'7' {
         return Err("Section 8 (End Section) mismatch".to_string());
@@ -190,7 +190,7 @@ pub fn unpack_sect8(f: &mut File) -> Result<(), String> {
 /// number and size.
 pub fn unpack_sect_header(f: &mut File) -> Result<(u8, usize), String> {
     let mut buf = [0; 5];
-    try!(f.read_exact(&mut buf[..]).map_err(clarify_err));
+    f.read_exact(&mut buf[..]).map_err(clarify_err)?;
 
     let sect_size = concat_bytes_as_u32(&mut buf, 0, 4) as usize;
     let sect_num = buf[4];
