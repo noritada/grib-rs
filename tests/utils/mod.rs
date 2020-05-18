79fb3c3feb016281db62bbe29fc8aa460f902ad0
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2020-05-18 22:51:42 +0900

    Cleanup: remove redundant code

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -4,27 +4,22 @@ use tempfile::NamedTempFile;
 use xz2::bufread::XzDecoder;
 
 pub(crate) fn jma_tornado_nowcast_file() -> Result<NamedTempFile, io::Error> {
-    let mut buf = Vec::new();
-    let mut out = NamedTempFile::new()?;
-
-    let f = File::open(
+    unxz_to_tempfile(
         "testdata/Z__C_RJTD_20160822020000_NOWC_GPV_Ggis10km_Pphw10_FH0000-0100_grib2.bin.xz",
-    )?;
-    let f = BufReader::new(f);
-    let mut f = XzDecoder::new(f);
-    f.read_to_end(&mut buf)?;
-    out.write_all(&buf)?;
-
-    Ok(out)
+    )
 }
 
 pub(crate) fn jma_kousa_file() -> Result<NamedTempFile, io::Error> {
+    unxz_to_tempfile(
+        "testdata/Z__C_RJTD_20170221120000_MSG_GPV_Gll0p5deg_Pys_B20170221120000_F2017022115-2017022212_grib2.bin.xz",
+    )
+}
+
+fn unxz_to_tempfile(file_name: &str) -> Result<NamedTempFile, io::Error> {
     let mut buf = Vec::new();
     let mut out = NamedTempFile::new()?;
 
-    let f = File::open(
-        "testdata/Z__C_RJTD_20170221120000_MSG_GPV_Gll0p5deg_Pys_B20170221120000_F2017022115-2017022212_grib2.bin.xz",
-    )?;
+    let f = File::open(file_name)?;
     let f = BufReader::new(f);
     let mut f = XzDecoder::new(f);
     f.read_to_end(&mut buf)?;
