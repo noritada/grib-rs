9e5bbfbdc2bf100e7a799ec23a5a3efa253951ab
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2021-05-02 10:51:32 +0900

    build: reorder input files for readability

--- build.rs
+++ build.rs
@@ -23,9 +23,9 @@ fn main() {
         "def/GRIB2/GRIB2_CodeFlag_1_3_CodeTable_en.csv",
         "def/GRIB2/GRIB2_CodeFlag_1_4_CodeTable_en.csv",
         "def/GRIB2/GRIB2_CodeFlag_3_1_CodeTable_en.csv",
+        "def/GRIB2/GRIB2_CodeFlag_4_0_CodeTable_en.csv",
         "def/GRIB2/GRIB2_CodeFlag_4_1_CodeTable_en.csv",
         "def/GRIB2/GRIB2_CodeFlag_4_2_CodeTable_en.csv",
-        "def/GRIB2/GRIB2_CodeFlag_4_0_CodeTable_en.csv",
         "def/GRIB2/GRIB2_CodeFlag_5_0_CodeTable_en.csv",
     ];
     let mut db = grib_build::grib2_codeflag_csv::CodeDB::new();
