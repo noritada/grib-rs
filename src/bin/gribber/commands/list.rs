41ad3b4880902e326386999b123b86924aad64e4
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2021-05-03 11:43:49 +0900

    cli: refactor the display of "list"

--- src/bin/gribber/commands/list.rs
+++ src/bin/gribber/commands/list.rs
@@ -1,4 +1,7 @@
 use clap::{App, Arg, ArgMatches, SubCommand};
+use std::fmt::{self, Display, Formatter};
+
+use grib::context::SubMessageIterator;
 
 use crate::cli;
 
@@ -11,8 +14,28 @@ pub fn cli() -> App<'static, 'static> {
 pub fn exec(args: &ArgMatches<'static>) -> Result<(), cli::CliError> {
     let file_name = args.value_of("file").unwrap();
     let grib = cli::grib(file_name)?;
-    for (i, submessage) in grib.submessages().enumerate() {
-        println!("{}\n{}", i, submessage.describe());
-    }
+
+    let view = ListView::new(grib.submessages());
+    print!("{}", view);
+
     Ok(())
+}
+
+struct ListView<'i> {
+    data: SubMessageIterator<'i>,
+}
+
+impl<'i> ListView<'i> {
+    fn new(data: SubMessageIterator<'i>) -> Self {
+        Self { data: data }
+    }
+}
+
+impl<'i> Display for ListView<'i> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        for (i, submessage) in self.data.clone().enumerate() {
+            write!(f, "{}\n{}\n", i, submessage.describe())?;
+        }
+        Ok(())
+    }
 }
