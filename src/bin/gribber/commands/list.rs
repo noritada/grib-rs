edab5045b3c69f692d5108e932cf5202018040df
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2021-05-03 11:54:19 +0900

    cli: make use of a pager to display the result of "list"

--- src/bin/gribber/commands/list.rs
+++ src/bin/gribber/commands/list.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg, ArgMatches, SubCommand};
+use console::Term;
 use std::fmt::{self, Display, Formatter};
 
 use grib::context::SubMessageIterator;
@@ -15,7 +16,14 @@ pub fn exec(args: &ArgMatches<'static>) -> Result<(), cli::CliError> {
     let file_name = args.value_of("file").unwrap();
     let grib = cli::grib(file_name)?;
 
-    let view = ListView::new(grib.submessages());
+    let view = ListView::new(grib.submessages(), ListViewMode::Dump);
+
+    let term = Term::stdout();
+    let (height, _width) = term.size();
+    if view.num_lines() > height.into() {
+        cli::start_pager();
+    }
+
     print!("{}", view);
 
     Ok(())
@@ -23,11 +31,26 @@ pub fn exec(args: &ArgMatches<'static>) -> Result<(), cli::CliError> {
 
 struct ListView<'i> {
     data: SubMessageIterator<'i>,
+    mode: ListViewMode,
 }
 
 impl<'i> ListView<'i> {
-    fn new(data: SubMessageIterator<'i>) -> Self {
-        Self { data: data }
+    fn new(data: SubMessageIterator<'i>, mode: ListViewMode) -> Self {
+        Self {
+            data: data,
+            mode: mode,
+        }
+    }
+
+    fn num_lines(&self) -> usize {
+        match self.mode {
+            ListViewMode::Dump => {
+                let unit_height = 8; // lines of output from SubMessage.describe(), hard-coded as of now
+                let (len, _) = self.data.size_hint();
+                let total_height = (unit_height + 2) * len - 1;
+                total_height
+            }
+        }
     }
 }
 
@@ -38,4 +61,8 @@ impl<'i> Display for ListView<'i> {
         }
         Ok(())
     }
+}
+
+enum ListViewMode {
+    Dump,
 }
