28fa82299c115f274a27a8dae6132d303d3f8876
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2021-05-20 23:32:39 +0900

    Add an example script to find surfaces with specified forecast time

---
+++ examples/find_surfaces.rs
@@ -0,0 +1,49 @@
+use grib::codetables::grib2::*;
+use grib::codetables::*;
+use grib::context::Grib2;
+use grib::datatypes::*;
+use grib::reader::SeekableGrib2Reader;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+fn main() {
+    // This example shows how to find surfaces in a GRIB2 message.
+
+    // Take the first argument as an input file path and the second argument as forecast time in hours.
+    let mut args = env::args();
+    let file_name = args.nth(1).unwrap();
+    let path = Path::new(&file_name);
+    let forecast_time = args.next().unwrap();
+    let forecast_time = forecast_time.parse::<u32>().unwrap();
+
+    find_surfaces(path, forecast_time)
+}
+
+fn find_surfaces(path: &Path, forecast_time_hours: u32) {
+    // Open the input file in a normal way, ignoring errors.
+    let f = File::open(&path).unwrap();
+    let f = BufReader::new(f);
+
+    // Read with the reader provided by the library. Errors are ignored in this case, too.
+    // This interface is ugly and will be improved in the future.
+    let grib2 = Grib2::<SeekableGrib2Reader<BufReader<File>>>::read_with_seekable(f).unwrap();
+
+    for (index, submessage) in grib2.iter().enumerate() {
+        let ft = submessage.prod_def().forecast_time();
+        match ft {
+            Some(ForecastTime {
+                unit: Found(Table4_4::Hour),
+                value: hours,
+            }) => {
+                if hours == forecast_time_hours {
+                    println!("{}: {}", index, hours);
+                }
+            }
+            _ => {}
+        }
+        //if ft.unit == Table4_4::Hours && ft.value == forecast_time_hours {
+        //}
+    }
+}
