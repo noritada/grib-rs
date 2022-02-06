7d09569059cf45bdf7dc1a2bb99393cfbc68a8c1
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-02-06 14:22:31 +0900

    Split up a validator out of `Grib2SubmessageStream` for future enhancements

--- src/parser.rs
+++ src/parser.rs
@@ -16,9 +16,9 @@ pub struct Submessage(
 
 ///
 /// # Example
-/// In every `Submessage`, the `offset` of all Section 8 is always set to 0
-/// since this parser only reads required submessages from the beginning of the
-/// file.
+/// In all but the last `Submessage` in a message, the `offset` of Section 8 is
+/// set to 0 since this parser only reads required submessages from the
+/// beginning of the file.
 ///
 /// ```
 /// use grib::context::{SectionBody, SectionInfo};
@@ -58,7 +58,7 @@ pub struct Submessage(
 ///             submessage.7.offset,
 ///             submessage.8.offset
 ///         ),
-///         (0, 16, Some(37), 64, 99, 157, 178, 184, 0)
+///         (0, 16, Some(37), 64, 99, 157, 178, 184, 189)
 ///     );
 ///
 ///     let second = parser.next();
@@ -70,15 +70,15 @@ pub struct Grib2SubmessageStream<I>
 where
     I: Iterator,
 {
-    iter: Peekable<Enumerate<I>>,
-    pos: usize,
-    message_count: usize,
-    submessage_count: usize,
+    iter: Grib2SubmessageValidator<I>,
     sect0: SectionInfo,
     sect1: SectionInfo,
     sect2: Option<SectionInfo>,
     sect3: SectionInfo,
-    terminated: bool,
+    sect4: SectionInfo,
+    sect5: SectionInfo,
+    sect6: SectionInfo,
+    sect7: SectionInfo,
 }
 
 impl<I> Grib2SubmessageStream<I>
@@ -103,45 +103,23 @@ where
     /// ```
     pub fn new(iter: I) -> Self {
         Self {
-            iter: iter.enumerate().peekable(),
-            pos: 0,
-            message_count: 0,
-            submessage_count: 0,
+            iter: Grib2SubmessageValidator::new(iter),
             sect0: Default::default(),
             sect1: Default::default(),
             sect2: Default::default(),
             sect3: Default::default(),
-            terminated: false,
+            sect4: Default::default(),
+            sect5: Default::default(),
+            sect6: Default::default(),
+            sect7: Default::default(),
         }
     }
 
-    fn clear_submessage_cache(&mut self) {
-        self.submessage_count = 0;
+    fn clear_message_cache(&mut self) {
         self.sect0 = Default::default();
         self.sect1 = Default::default();
         self.sect2 = Default::default();
         self.sect3 = Default::default();
-    }
-
-    fn new_unexpected_end_of_data_err(
-        &mut self,
-    ) -> Option<Result<(usize, usize, Submessage), ParseError>> {
-        self.terminated = true;
-        Some(Err(ParseError::UnexpectedEndOfData(self.pos)))
-    }
-
-    fn new_invalid_section_order_err(
-        &mut self,
-    ) -> Option<Result<(usize, usize, Submessage), ParseError>> {
-        self.terminated = true;
-        Some(Err(ParseError::InvalidSectionOrder(self.pos)))
-    }
-
-    fn new_no_grid_definition_err(
-        &mut self,
-    ) -> Option<Result<(usize, usize, Submessage), ParseError>> {
-        self.terminated = true;
-        Some(Err(ParseError::NoGridDefinition(self.pos)))
     }
 }
 
@@ -152,127 +130,283 @@ where
     type Item = Result<(usize, usize, Submessage), ParseError>;
 
     fn next(&mut self) -> Option<Result<(usize, usize, Submessage), ParseError>> {
-        macro_rules! ensure_next_sect_num {
-            ($num:expr) => {{
-                match self.iter.next() {
-                    Some((pos, Ok(s))) => {
-                        self.pos = pos;
-                        if s.num == $num {
-                            s
-                        } else {
-                            return self.new_invalid_section_order_err();
-                        }
-                    }
-                    Some((_, Err(e))) => {
-                        return Some(Err(e));
-                    }
-                    None => {
-                        return self.new_unexpected_end_of_data_err();
-                    }
-                }
-            }};
-        }
-
-        if self.terminated {
-            return None;
-        }
-
-        if self.submessage_count == 0 {
+        loop {
             match self.iter.next()? {
-                (pos, Ok(s)) => {
-                    self.pos = pos;
-                    if s.num == 0 {
-                        self.sect0 = s
-                    } else {
-                        return self.new_invalid_section_order_err();
+                Err(e) => return Some(Err(e)),
+                Ok((pos, message_count, submessage_count, s)) => match s.num {
+                    0 => {
+                        self.sect0 = s;
                     }
-                }
-                (_, Err(e)) => {
-                    return Some(Err(e));
+                    1 => {
+                        self.sect1 = s;
+                    }
+                    2 => {
+                        self.sect2 = Some(s);
+                    }
+                    3 => {
+                        self.sect3 = s;
+                    }
+                    4 => {
+                        self.sect4 = s;
+                    }
+                    5 => {
+                        self.sect5 = s;
+                    }
+                    6 => {
+                        self.sect6 = s;
+                    }
+                    7 => {
+                        self.sect7 = s;
+                    }
+                    8 => {
+                        let ret = Some(Ok((
+                            message_count,
+                            submessage_count,
+                            Submessage(
+                                self.sect0.clone(),
+                                self.sect1.clone(),
+                                self.sect2.clone(),
+                                self.sect3.clone(),
+                                self.sect4.clone(),
+                                self.sect5.clone(),
+                                self.sect6.clone(),
+                                self.sect7.clone(),
+                                s,
+                            ),
+                        )));
+
+                        // if pos is 0, it is dummy
+                        if pos != 0 {
+                            self.clear_message_cache();
+                        }
+                        return ret;
+                    }
+                    _ => unreachable!(),
+                },
+            }
+        }
+    }
+}
+
+struct Grib2SubmessageValidator<I>
+where
+    I: Iterator,
+{
+    iter: Peekable<Enumerate<I>>,
+    pos: usize,
+    message_count: usize,
+    submessage_count: usize,
+    state: Grib2SubmessageValidatorState,
+    has_sect3: bool,
+}
+
+impl<I> Grib2SubmessageValidator<I>
+where
+    I: Iterator,
+{
+    pub fn new(iter: I) -> Self {
+        Grib2SubmessageValidator {
+            iter: iter.enumerate().peekable(),
+            pos: 0,
+            message_count: 0,
+            submessage_count: 0,
+            state: Grib2SubmessageValidatorState::StartOfMessage,
+            has_sect3: false,
+        }
+    }
+
+    #[inline]
+    fn reinitialize_message(&mut self) {
+        self.submessage_count = 0;
+        self.state = Grib2SubmessageValidatorState::StartOfMessage;
+        self.has_sect3 = false;
+    }
+
+    fn new_unexpected_end_of_data_err(
+        &mut self,
+    ) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        self.state = Grib2SubmessageValidatorState::EndOfStream;
+        Some(Err(ParseError::UnexpectedEndOfData(self.pos)))
+    }
+
+    fn new_invalid_section_order_err(
+        &mut self,
+    ) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        self.state = Grib2SubmessageValidatorState::EndOfStream;
+        Some(Err(ParseError::InvalidSectionOrder(self.pos)))
+    }
+
+    fn new_no_grid_definition_err(
+        &mut self,
+    ) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        self.state = Grib2SubmessageValidatorState::EndOfStream;
+        Some(Err(ParseError::NoGridDefinition(self.pos)))
+    }
+
+    fn wrap_parse_err(
+        &mut self,
+        e: ParseError,
+    ) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        self.state = Grib2SubmessageValidatorState::EndOfStream;
+        Some(Err(e))
+    }
+}
+
+impl<I> Grib2SubmessageValidator<I>
+where
+    I: Iterator<Item = Result<SectionInfo, ParseError>>,
+{
+    #[inline]
+    fn ensure_next_is_sect0(
+        &mut self,
+    ) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        match self.iter.next()? {
+            (pos, Ok(s)) => {
+                self.pos = pos;
+                if s.num == 0 {
+                    self.state = Grib2SubmessageValidatorState::EndOfSect(0);
+                    Some(Ok((self.pos, self.message_count, self.submessage_count, s)))
+                } else {
+                    self.new_invalid_section_order_err()
                 }
             }
-
-            self.sect1 = ensure_next_sect_num!(1);
+            (_, Err(e)) => self.wrap_parse_err(e),
         }
+    }
 
-        let sect = match self.iter.next() {
+    #[inline]
+    fn ensure_next_is_sect_2_or_3_or_4(
+        &mut self,
+    ) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        match self.iter.next() {
             Some((pos, Ok(s))) => {
                 self.pos = pos;
-                s
-            }
-            Some((_, Err(e))) => {
-                return Some(Err(e));
-            }
-            _ => {
-                return self.new_unexpected_end_of_data_err();
-            }
-        };
-
-        let sect4 = match sect.num {
-            2 => {
-                self.sect2 = Some(sect);
-                self.sect3 = ensure_next_sect_num!(3);
-                ensure_next_sect_num!(4)
-            }
-            3 => {
-                self.sect3 = sect;
-                ensure_next_sect_num!(4)
-            }
-            4 => {
-                if self.sect3.num == 0 {
-                    return self.new_no_grid_definition_err();
+                match s.num {
+                    2 => {
+                        self.state = Grib2SubmessageValidatorState::EndOfSect(2);
+                        Some(Ok((self.pos, self.message_count, self.submessage_count, s)))
+                    }
+                    3 => {
+                        self.state = Grib2SubmessageValidatorState::EndOfSect(3);
+                        self.has_sect3 = true;
+                        Some(Ok((self.pos, self.message_count, self.submessage_count, s)))
+                    }
+                    4 => {
+                        if self.has_sect3 {
+                            self.state = Grib2SubmessageValidatorState::EndOfSect(4);
+                            Some(Ok((self.pos, self.message_count, self.submessage_count, s)))
+                        } else {
+                            self.new_no_grid_definition_err()
+                        }
+                    }
+                    _ => self.new_invalid_section_order_err(),
                 }
-                sect
             }
-            _ => {
-                return self.new_invalid_section_order_err();
-            }
-        };
+            Some((_, Err(e))) => self.wrap_parse_err(e),
+            None => self.new_unexpected_end_of_data_err(),
+        }
+    }
 
-        let sect5 = ensure_next_sect_num!(5);
-        let sect6 = ensure_next_sect_num!(6);
-        let sect7 = ensure_next_sect_num!(7);
-
-        let next_value = Some(Ok((
-            self.message_count,
-            self.submessage_count,
-            Submessage(
-                self.sect0.clone(),
-                self.sect1.clone(),
-                self.sect2.clone(),
-                self.sect3.clone(),
-                sect4,
-                sect5,
-                sect6,
-                sect7,
-                SectionInfo::new_8(0),
-            ),
-        )));
-
-        let sect8 = self.iter.peek();
-        match sect8 {
-            None => {
-                return self.new_unexpected_end_of_data_err();
-            }
+    #[inline]
+    fn ensure_next_is_sect8(
+        &mut self,
+    ) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        match self.iter.peek() {
             Some((_, Err(_))) => {
                 if let Some((_, Err(e))) = self.iter.next() {
-                    return Some(Err(e));
+                    self.wrap_parse_err(e)
                 } else {
                     unreachable!()
                 }
             }
             Some((_, Ok(SectionInfo { num: 8, .. }))) => {
-                self.iter.next();
+                let sect = if let Some((pos, Ok(s))) = self.iter.next() {
+                    self.pos = pos;
+                    s
+                } else {
+                    unreachable!()
+                };
+                let ret = Some(Ok((
+                    self.pos,
+                    self.message_count,
+                    self.submessage_count,
+                    sect,
+                )));
+
                 self.message_count += 1;
-                self.clear_submessage_cache();
+                self.reinitialize_message();
+                ret
             }
             Some((_, Ok(_))) => {
+                // return dummy Section 8
+                let ret = Some(Ok((
+                    0,
+                    self.message_count,
+                    self.submessage_count,
+                    SectionInfo::new_8(0),
+                )));
                 self.submessage_count += 1;
+                self.state = Grib2SubmessageValidatorState::StartOfSubmessage;
+                ret
             }
+            None => self.new_unexpected_end_of_data_err(),
         }
-
-        next_value
     }
+
+    fn ensure_next_is_sect_num(
+        &mut self,
+        num: u8,
+    ) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        match self.iter.next() {
+            Some((pos, Ok(s))) => {
+                self.state = Grib2SubmessageValidatorState::EndOfSect(num);
+                self.pos = pos;
+                if s.num == num {
+                    Some(Ok((self.pos, self.message_count, self.submessage_count, s)))
+                } else {
+                    self.new_invalid_section_order_err()
+                }
+            }
+            Some((_, Err(e))) => self.wrap_parse_err(e),
+            None => self.new_unexpected_end_of_data_err(),
+        }
+    }
+}
+
+impl<I> Iterator for Grib2SubmessageValidator<I>
+where
+    I: Iterator<Item = Result<SectionInfo, ParseError>>,
+{
+    type Item = Result<(usize, usize, usize, SectionInfo), ParseError>;
+
+    fn next(&mut self) -> Option<Result<(usize, usize, usize, SectionInfo), ParseError>> {
+        match self.state {
+            Grib2SubmessageValidatorState::EndOfStream => None,
+            Grib2SubmessageValidatorState::StartOfMessage => self.ensure_next_is_sect0(),
+            Grib2SubmessageValidatorState::EndOfSect(0) => self.ensure_next_is_sect_num(1),
+            Grib2SubmessageValidatorState::EndOfSect(1) => self.ensure_next_is_sect_2_or_3_or_4(),
+            Grib2SubmessageValidatorState::StartOfSubmessage => {
+                self.ensure_next_is_sect_2_or_3_or_4()
+            }
+            Grib2SubmessageValidatorState::EndOfSect(2) => {
+                self.has_sect3 = true;
+                self.ensure_next_is_sect_num(3)
+            }
+            Grib2SubmessageValidatorState::EndOfSect(3) => self.ensure_next_is_sect_num(4),
+            Grib2SubmessageValidatorState::EndOfSect(4) => self.ensure_next_is_sect_num(5),
+            Grib2SubmessageValidatorState::EndOfSect(5) => self.ensure_next_is_sect_num(6),
+            Grib2SubmessageValidatorState::EndOfSect(6) => self.ensure_next_is_sect_num(7),
+            Grib2SubmessageValidatorState::EndOfSect(7) => self.ensure_next_is_sect8(),
+            Grib2SubmessageValidatorState::EndOfSect(_) => unreachable!(),
+        }
+    }
+}
+
+enum Grib2SubmessageValidatorState {
+    StartOfMessage,
+    StartOfSubmessage,
+    EndOfSect(u8),
+    EndOfStream,
 }
 
 #[cfg(test)]
@@ -331,7 +465,7 @@ mod tests {
             Grib2SubmessageStream::new(sects.into_iter())
                 .map(|result| result.map(|i| digest_submessage_iter_item(i)))
                 .collect::<Vec<_>>(),
-            vec![Ok((0, 0, 0, 1, Some(2), 3, 4, 5, 6, 7, 0))],
+            vec![Ok((0, 0, 0, 1, Some(2), 3, 4, 5, 6, 7, 8))],
         );
     }
 
@@ -346,8 +480,8 @@ mod tests {
                 .map(|result| result.map(|i| digest_submessage_iter_item(i)))
                 .collect::<Vec<_>>(),
             vec![
-                Ok((0, 0, 0, 1, Some(2), 3, 4, 5, 6, 7, 0)),
-                Ok((1, 0, 9, 10, Some(11), 12, 13, 14, 15, 16, 0))
+                Ok((0, 0, 0, 1, Some(2), 3, 4, 5, 6, 7, 8)),
+                Ok((1, 0, 9, 10, Some(11), 12, 13, 14, 15, 16, 17))
             ],
         );
     }
@@ -374,8 +508,8 @@ mod tests {
                 .map(|result| result.map(|i| digest_submessage_iter_item(i)))
                 .collect::<Vec<_>>(),
             vec![
-                Ok((0, 0, 0, 1, None, 2, 3, 4, 5, 6, 0)),
-                Ok((1, 0, 8, 9, None, 10, 11, 12, 13, 14, 0))
+                Ok((0, 0, 0, 1, None, 2, 3, 4, 5, 6, 7)),
+                Ok((1, 0, 8, 9, None, 10, 11, 12, 13, 14, 15))
             ],
         );
     }
@@ -391,7 +525,7 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec![
                 Ok((0, 0, 0, 1, Some(2), 3, 4, 5, 6, 7, 0)),
-                Ok((0, 1, 0, 1, Some(8), 9, 10, 11, 12, 13, 0))
+                Ok((0, 1, 0, 1, Some(8), 9, 10, 11, 12, 13, 14))
             ],
         );
     }
@@ -406,7 +540,7 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec![
                 Ok((0, 0, 0, 1, None, 2, 3, 4, 5, 6, 0)),
-                Ok((0, 1, 0, 1, Some(7), 8, 9, 10, 11, 12, 0))
+                Ok((0, 1, 0, 1, Some(7), 8, 9, 10, 11, 12, 13))
             ],
         );
     }
@@ -421,7 +555,7 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec![
                 Ok((0, 0, 0, 1, Some(2), 3, 4, 5, 6, 7, 0)),
-                Ok((0, 1, 0, 1, Some(2), 8, 9, 10, 11, 12, 0))
+                Ok((0, 1, 0, 1, Some(2), 8, 9, 10, 11, 12, 13))
             ],
         );
     }
@@ -436,7 +570,7 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec![
                 Ok((0, 0, 0, 1, None, 2, 3, 4, 5, 6, 0)),
-                Ok((0, 1, 0, 1, None, 7, 8, 9, 10, 11, 0))
+                Ok((0, 1, 0, 1, None, 7, 8, 9, 10, 11, 12))
             ],
         );
     }
@@ -450,7 +584,7 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec![
                 Ok((0, 0, 0, 1, Some(2), 3, 4, 5, 6, 7, 0)),
-                Ok((0, 1, 0, 1, Some(2), 3, 8, 9, 10, 11, 0))
+                Ok((0, 1, 0, 1, Some(2), 3, 8, 9, 10, 11, 12))
             ],
         );
     }
@@ -465,7 +599,7 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec![
                 Ok((0, 0, 0, 1, None, 2, 3, 4, 5, 6, 0)),
-                Ok((0, 1, 0, 1, None, 2, 7, 8, 9, 10, 0))
+                Ok((0, 1, 0, 1, None, 2, 7, 8, 9, 10, 11))
             ],
         );
     }
@@ -483,8 +617,8 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec![
                 Ok((0, 0, 0, 1, Some(2), 3, 4, 5, 6, 7, 0)),
-                Ok((0, 1, 0, 1, Some(8), 9, 10, 11, 12, 13, 0)),
-                Ok((1, 0, 15, 16, None, 17, 18, 19, 20, 21, 0))
+                Ok((0, 1, 0, 1, Some(8), 9, 10, 11, 12, 13, 14)),
+                Ok((1, 0, 15, 16, None, 17, 18, 19, 20, 21, 22))
             ],
         );
     }
