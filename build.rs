9a620d06d3df00c81203cea244bd893a2d4e080f
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-08-03 23:52:11 +0900

    build: use `AsRef<Path>` for argument types to simplify internal APIs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
@@ -8,10 +8,9 @@ fn main() {
     let input_file_names = ["def/CCT/C00.csv", "def/CCT/C11.csv"];
     let mut db = grib_build::cct_csv::CodeDB::new();
     let output_path = Path::new(&out_dir).join("cct.rs");
-    for file_name in &input_file_names {
-        let path = PathBuf::from(file_name);
+    for path in &input_file_names {
         db.load(path).unwrap();
-        println!("cargo:rerun-if-changed={}", file_name);
+        println!("cargo:rerun-if-changed={}", path);
     }
     fs::write(&output_path, format!("{}", db)).unwrap();
 
@@ -32,10 +31,9 @@ fn main() {
     ];
     let mut db = grib_build::grib2_codeflag_csv::CodeDB::new();
     let output_path = Path::new(&out_dir).join("grib2_codeflag.rs");
-    for file_name in &input_file_names {
-        let path = PathBuf::from(file_name);
+    for path in &input_file_names {
         db.load(path).unwrap();
-        println!("cargo:rerun-if-changed={}", file_name);
+        println!("cargo:rerun-if-changed={}", path);
     }
     fs::write(&output_path, format!("{}", db)).unwrap();
 
