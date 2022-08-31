7e1abed7c44bdb9ba6c9d9da355b94467dfce2f6
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-09-01 01:37:09 +0900

    Use a macro to eliminate duplication and clarify differences between test cases for CLI's "inspect" subcommand

--- tests/cli/main.rs
+++ tests/cli/main.rs
@@ -305,12 +305,29 @@ Data Representation:                    Run length packing with level values
     ),
 }
 
-#[test]
-fn inspect() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_tornado_nowcast_file()?;
-    let arg_path = tempfile.path();
+macro_rules! test_inspect_subcommand {
+    ($(($name:ident, $input:expr, $args:expr, $expected:expr),)*) => ($(
+        #[test]
+        fn $name() -> Result<(), Box<dyn std::error::Error>> {
+            let input = $input;
+            let mut cmd = Command::cargo_bin(CMD_NAME)?;
+            cmd.arg("inspect").args($args).arg(input.path());
+            cmd.assert()
+                .success()
+                .stdout(predicate::str::diff($expected))
+                .stderr(predicate::str::is_empty());
 
-    let out_str = "\
+            Ok(())
+        }
+    )*);
+}
+
+test_inspect_subcommand! {
+    (
+        inspect_without_options,
+        utils::jma_tornado_nowcast_file()?,
+        Vec::<&str>::new(),
+        "\
 Sections:
     0 │ 0000000000000000 - 0000000000000010 │ Section 0
     1 │ 0000000000000010 - 0000000000000025 │ Section 1
@@ -359,24 +376,13 @@ Templates:
 3.0      - Latitude/longitude
 4.0      - Analysis or forecast at a horizontal level or in a horizontal layer at a point in time
 5.200    - Run length packing with level values
-";
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("inspect").arg(arg_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::diff(out_str))
-        .stderr(predicate::str::is_empty());
-
-    Ok(())
-}
-
-#[test]
-fn inspect_with_opt_s() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_tornado_nowcast_file()?;
-    let arg_path = tempfile.path();
-
-    let out_str = "    0 │ 0000000000000000 - 0000000000000010 │ Section 0
+"
+    ),
+    (
+        inspect_with_opt_s,
+        utils::jma_tornado_nowcast_file()?,
+        vec!["-s"],
+        "    0 │ 0000000000000000 - 0000000000000010 │ Section 0
     1 │ 0000000000000010 - 0000000000000025 │ Section 1
     2 │ 0000000000000025 - 000000000000006d │ Section 3
     3 │ 000000000000006d - 000000000000008f │ Section 4
@@ -408,24 +414,13 @@ fn inspect_with_opt_s() -> Result<(), Box<dyn std::error::Error>> {
    29 │ 00000000000022dd - 00000000000022e3 │ Section 6
    30 │ 00000000000022e3 - 000000000000284d │ Section 7
    31 │ 000000000000284d - 0000000000002851 │ Section 8
-";
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("inspect").arg("-s").arg(arg_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::diff(out_str))
-        .stderr(predicate::str::is_empty());
-
-    Ok(())
-}
-
-#[test]
-fn inspect_with_opt_m() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_tornado_nowcast_file()?;
-    let arg_path = tempfile.path();
-
-    let out_str = "      id │    S2    S3    S4    S5    S6    S7 │ Tmpl3   Tmpl4   Tmpl5  
+"
+    ),
+    (
+        inspect_with_opt_m,
+        utils::jma_tornado_nowcast_file()?,
+        vec!["-m"],
+        "      id │    S2    S3    S4    S5    S6    S7 │ Tmpl3   Tmpl4   Tmpl5  
      0.0 │     -     2     3     4     5     6 │ 3.0     4.0     5.200  
      0.1 │     -     2     7     8     9    10 │ 3.0     4.0     5.200  
      0.2 │     -     2    11    12    13    14 │ 3.0     4.0     5.200  
@@ -433,41 +428,22 @@ fn inspect_with_opt_m() -> Result<(), Box<dyn std::error::Error>> {
      0.4 │     -     2    19    20    21    22 │ 3.0     4.0     5.200  
      0.5 │     -     2    23    24    25    26 │ 3.0     4.0     5.200  
      0.6 │     -     2    27    28    29    30 │ 3.0     4.0     5.200  
-";
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("inspect").arg("-m").arg(arg_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::diff(out_str))
-        .stderr(predicate::str::is_empty());
-
-    Ok(())
-}
-
-#[test]
-fn inspect_with_opt_t() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_tornado_nowcast_file()?;
-    let arg_path = tempfile.path();
-
-    let out_str = "\
+"
+    ),
+    (
+        inspect_with_opt_t,
+        utils::jma_tornado_nowcast_file()?,
+        vec!["-t"],
+        "\
 3.0      - Latitude/longitude
 4.0      - Analysis or forecast at a horizontal level or in a horizontal layer at a point in time
 5.200    - Run length packing with level values
-";
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("inspect").arg("-t").arg(arg_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::diff(out_str))
-        .stderr(predicate::str::is_empty());
-
-    Ok(())
+"
+    ),
 }
 
 #[test]
-fn inspect_with_all_opts() -> Result<(), Box<dyn std::error::Error>> {
+fn inspect_with_all_options() -> Result<(), Box<dyn std::error::Error>> {
     let tempfile = utils::jma_tornado_nowcast_file()?;
     let arg_path = tempfile.path();
 
