d79ec96b122b07ff10a27bbf2265ebea90e258c2
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-08-31 20:11:40 +0900

    Use macros to eliminate duplication and clarify differences between test cases for CLI's "decode" subcommand

--- tests/cli/main.rs
+++ tests/cli/main.rs
@@ -490,40 +490,90 @@ fn inspect_with_all_opts() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[test]
-fn decode_run_length_encoding() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_tornado_nowcast_file()?;
-    let arg_path = tempfile.path();
+macro_rules! test_decode_subcommand_with_no_options {
+    ($(($name:ident, $input:expr, $message_index:expr),)*) => ($(
+        #[test]
+        fn $name() -> Result<(), Box<dyn std::error::Error>> {
+            let input = $input;
 
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("decode").arg(arg_path).arg("0.3");
-    cmd.assert().success().stderr(predicate::str::is_empty());
+            let mut cmd = Command::cargo_bin(CMD_NAME)?;
+            cmd.arg("decode").arg(input.path()).arg($message_index);
+            cmd.assert().success().stderr(predicate::str::is_empty());
 
-    Ok(())
+            Ok(())
+        }
+    )*);
 }
 
-#[test]
-fn decode_simple_packing() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_kousa_file()?;
-    let arg_path = tempfile.path();
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("decode").arg(arg_path).arg("0.3");
-    cmd.assert().success().stderr(predicate::str::is_empty());
-
-    Ok(())
+test_decode_subcommand_with_no_options! {
+    (
+        decode_simple_packing,
+        utils::jma_kousa_file()?,
+        "0.3"
+    ),
+    (
+        decode_complex_packing,
+        utils::jma_meps_file()?,
+        "0.2"
+    ),
+    (
+        decode_run_length_encoding,
+        utils::jma_tornado_nowcast_file()?,
+        "0.3"
+    ),
 }
 
-#[test]
-fn decode_complex_packing() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_meps_file()?;
-    let arg_path = tempfile.path();
+macro_rules! test_decode_subcommand_with_data_without_nan_values_and_byte_order_options {
+    ($(($name:ident, $input:expr, $message_index:expr, $byte_order_flag:expr, $expected:expr),)*) => ($(
+        #[test]
+        fn $name() -> Result<(), Box<dyn std::error::Error>> {
+            let input = $input;
 
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("decode").arg(arg_path).arg("0.2");
-    cmd.assert().success().stderr(predicate::str::is_empty());
+            let dir = TempDir::new()?;
+            let out_path = dir.path().join("out.bin");
+            let out_path = format!("{}", out_path.display());
 
-    Ok(())
+            let mut cmd = Command::cargo_bin(CMD_NAME)?;
+            cmd.arg("decode")
+                .arg(input.path())
+                .arg($message_index)
+                .arg($byte_order_flag)
+                .arg(&out_path);
+            cmd.assert()
+                .success()
+                .stdout(predicate::str::is_empty())
+                .stderr(predicate::str::is_empty());
+
+            let actual = utils::cat_as_bytes(&out_path)?;
+            assert_eq!(actual, $expected);
+
+            Ok(())
+        }
+    )*);
+}
+
+test_decode_subcommand_with_data_without_nan_values_and_byte_order_options! {
+    (
+        decode_simple_packing_as_big_endian,
+        utils::jma_kousa_file()?,
+        "0.3",
+        "-b",
+        utils::kousa_be_bin_bytes()?
+    ),
+    (
+        decode_simple_packing_as_little_endian,
+        utils::jma_kousa_file()?,
+        "0.3",
+        "-l",
+        utils::kousa_le_bin_bytes()?
+    ),
+    (
+        decode_complex_packing_as_little_endian,
+        utils::jma_meps_file()?,
+        "0.2",
+        "-l",
+        utils::meps_le_bin_bytes()?
+    ),
 }
 
 #[test]
@@ -561,120 +611,58 @@ fn decode_run_length_packing_as_big_endian() -> Result<(), Box<dyn std::error::E
     Ok(())
 }
 
-#[test]
-fn decode_run_length_packing_as_little_endian() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_tornado_nowcast_file()?;
-    let arg_path = tempfile.path();
+macro_rules! test_decode_subcommand_with_data_with_nan_values_as_little_endian {
+    ($(($name:ident, $input:expr, $message_index:expr, $byte_order_flag:expr, $expected:expr),)*) => ($(
+        #[test]
+        fn $name() -> Result<(), Box<dyn std::error::Error>> {
+            let input = $input;
 
-    let dir = TempDir::new()?;
-    let out_path = dir.path().join("out.bin");
-    let out_path = format!("{}", out_path.display());
+            let dir = TempDir::new()?;
+            let out_path = dir.path().join("out.bin");
+            let out_path = format!("{}", out_path.display());
 
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("decode")
-        .arg(arg_path)
-        .arg("0.3")
-        .arg("-l")
-        .arg(&out_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::is_empty())
-        .stderr(predicate::str::is_empty());
+            let mut cmd = Command::cargo_bin(CMD_NAME)?;
+            cmd.arg("decode")
+                .arg(input.path())
+                .arg($message_index)
+                .arg($byte_order_flag)
+                .arg(&out_path);
+            cmd.assert()
+                .success()
+                .stdout(predicate::str::is_empty())
+                .stderr(predicate::str::is_empty());
 
-    let expected = utils::tornado_nowcast_le_bin_bytes()?;
-    let expected: Vec<_> = expected
-        .chunks(4)
-        .into_iter()
-        .flat_map(|b| match b {
-            [0x9a, 0xd1, 0x58, 0x62] => vec![0x00, 0x00, 0xc0, 0x7f],
-            b => b.to_vec(),
-        })
-        .collect();
-    let actual = utils::cat_as_bytes(&out_path)?;
-    assert_eq!(actual, expected);
+            let expected: Vec<_> = $expected
+                .chunks(4)
+                .into_iter()
+                .flat_map(|b| match b {
+                    [0x9a, 0xd1, 0x58, 0x62] => vec![0x00, 0x00, 0xc0, 0x7f],
+                    b => b.to_vec(),
+                })
+                .collect();
+            let actual = utils::cat_as_bytes(&out_path)?;
+            assert_eq!(actual, expected);
 
-    Ok(())
+            Ok(())
+        }
+    )*);
 }
 
-#[test]
-fn decode_simple_packing_as_big_endian() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_kousa_file()?;
-    let arg_path = tempfile.path();
-
-    let dir = TempDir::new()?;
-    let out_path = dir.path().join("out.bin");
-    let out_path = format!("{}", out_path.display());
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("decode")
-        .arg(arg_path)
-        .arg("0.3")
-        .arg("-b")
-        .arg(&out_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::is_empty())
-        .stderr(predicate::str::is_empty());
-
-    let expected = utils::kousa_be_bin_bytes()?;
-    let actual = utils::cat_as_bytes(&out_path)?;
-    assert_eq!(actual, expected);
-
-    Ok(())
-}
-
-#[test]
-fn decode_simple_packing_as_little_endian() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_kousa_file()?;
-    let arg_path = tempfile.path();
-
-    let dir = TempDir::new()?;
-    let out_path = dir.path().join("out.bin");
-    let out_path = format!("{}", out_path.display());
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("decode")
-        .arg(arg_path)
-        .arg("0.3")
-        .arg("-l")
-        .arg(&out_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::is_empty())
-        .stderr(predicate::str::is_empty());
-
-    let expected = utils::kousa_le_bin_bytes()?;
-    let actual = utils::cat_as_bytes(&out_path)?;
-    assert_eq!(actual, expected);
-
-    Ok(())
-}
-
-#[test]
-fn decode_complex_packing_as_little_endian() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_meps_file()?;
-    let arg_path = tempfile.path();
-
-    let dir = TempDir::new()?;
-    let out_path = dir.path().join("out.bin");
-    let out_path = format!("{}", out_path.display());
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("decode")
-        .arg(arg_path)
-        .arg("0.2")
-        .arg("-l")
-        .arg(&out_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::is_empty())
-        .stderr(predicate::str::is_empty());
-
-    let expected = utils::meps_le_bin_bytes()?;
-    let actual = utils::cat_as_bytes(&out_path)?;
-    assert_eq!(actual, expected);
-
-    Ok(())
+test_decode_subcommand_with_data_with_nan_values_as_little_endian! {
+    (
+        decode_run_length_packing_as_little_endian,
+        utils::jma_tornado_nowcast_file()?,
+        "0.3",
+        "-l",
+        utils::tornado_nowcast_le_bin_bytes()?
+    ),
+    (
+        decode_simple_packing_with_bitmap_as_little_endian,
+        utils::jma_msmguid_file()?,
+        "0.0",
+        "-l",
+        utils::msmguid_le_bin_bytes()?
+    ),
 }
 
 #[test]
@@ -705,41 +693,6 @@ fn decode_jpeg2000_code_stream_as_little_endian() -> Result<(), Box<dyn std::err
     let expected = utils::encode_le_bytes_using_simple_packing(expected, ref_val, exp, dig);
     let actual = utils::cat_as_bytes(&out_path)?;
     let actual = utils::encode_le_bytes_using_simple_packing(actual, ref_val, exp, dig);
-    assert_eq!(actual, expected);
-
-    Ok(())
-}
-
-#[test]
-fn decode_simple_packing_with_bitmap_as_little_endian() -> Result<(), Box<dyn std::error::Error>> {
-    let tempfile = utils::jma_msmguid_file()?;
-    let arg_path = tempfile.path();
-
-    let dir = TempDir::new()?;
-    let out_path = dir.path().join("out.bin");
-    let out_path = format!("{}", out_path.display());
-
-    let mut cmd = Command::cargo_bin(CMD_NAME)?;
-    cmd.arg("decode")
-        .arg(arg_path)
-        .arg("0.0")
-        .arg("-l")
-        .arg(&out_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::is_empty())
-        .stderr(predicate::str::is_empty());
-
-    let expected = utils::msmguid_le_bin_bytes()?;
-    let expected: Vec<_> = expected
-        .chunks(4)
-        .into_iter()
-        .flat_map(|b| match b {
-            [0x9a, 0xd1, 0x58, 0x62] => vec![0x00, 0x00, 0xc0, 0x7f],
-            b => b.to_vec(),
-        })
-        .collect();
-    let actual = utils::cat_as_bytes(&out_path)?;
     assert_eq!(actual, expected);
 
     Ok(())
