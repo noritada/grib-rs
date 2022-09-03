e484a482e21fd61359767d939fea908e4bc673ee
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-09-03 16:43:46 +0900

    cli: prepare to add multi-message data support for the "info" subcommand

--- src/bin/gribber/commands/info.rs
+++ src/bin/gribber/commands/info.rs
@@ -19,53 +19,43 @@ pub fn cli() -> Command<'static> {
 pub fn exec(args: &ArgMatches) -> anyhow::Result<()> {
     let file_name = args.get_one::<PathBuf>("FILE").unwrap();
     let grib = cli::grib(file_name)?;
-    let info = InfoItem::new(grib.info()?);
+    let (indicator, identification) = grib.info()?;
+    let info = InfoView(indicator, identification);
     print!("{}", info);
     Ok(())
 }
 
-struct InfoItem<'i> {
-    indicator: &'i Indicator,
-    identification: &'i Identification,
-}
+struct InfoView<'i>(&'i Indicator, &'i Identification);
 
-impl<'i> InfoItem<'i> {
-    fn new(info: (&'i Indicator, &'i Identification)) -> Self {
-        Self {
-            indicator: info.0,
-            identification: info.1,
-        }
-    }
-}
-
-impl<'i> Display for InfoItem<'i> {
+impl<'i> Display for InfoView<'i> {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let Self(indicator, identification) = self;
         write!(
             f,
             "\
-Discipline:                             {}
-Total Length:                           {}
-Originating/generating centre:          {}
-Originating/generating sub-centre:      {}
-GRIB Master Tables Version Number:      {} ({})
-GRIB Local Tables Version Number:       {} ({})
-Significance of Reference Time:         {}
-Reference time of data:                 {}
-Production status of processed data:    {}
-Type of processed data:                 {}
+    Discipline:                             {}
+    Total Length:                           {}
+    Originating/generating centre:          {}
+    Originating/generating sub-centre:      {}
+    GRIB Master Tables Version Number:      {} ({})
+    GRIB Local Tables Version Number:       {} ({})
+    Significance of Reference Time:         {}
+    Reference time of data:                 {}
+    Production status of processed data:    {}
+    Type of processed data:                 {}
 ",
-            CodeTable0_0.lookup(self.indicator.discipline as usize),
-            self.indicator.total_length,
-            CommonCodeTable11.lookup(self.identification.centre_id() as usize),
-            self.identification.subcentre_id(),
-            self.identification.master_table_version(),
-            CommonCodeTable00.lookup(self.identification.master_table_version() as usize),
-            self.identification.local_table_version(),
-            CodeTable1_1.lookup(self.identification.local_table_version() as usize),
-            CodeTable1_2.lookup(self.identification.ref_time_significance() as usize),
-            self.identification.ref_time(),
-            CodeTable1_3.lookup(self.identification.prod_status() as usize),
-            CodeTable1_4.lookup(self.identification.data_type() as usize)
+            CodeTable0_0.lookup(indicator.discipline as usize),
+            indicator.total_length,
+            CommonCodeTable11.lookup(identification.centre_id() as usize),
+            identification.subcentre_id(),
+            identification.master_table_version(),
+            CommonCodeTable00.lookup(identification.master_table_version() as usize),
+            identification.local_table_version(),
+            CodeTable1_1.lookup(identification.local_table_version() as usize),
+            CodeTable1_2.lookup(identification.ref_time_significance() as usize),
+            identification.ref_time(),
+            CodeTable1_3.lookup(identification.prod_status() as usize),
+            CodeTable1_4.lookup(identification.data_type() as usize)
         )
     }
 }
--- tests/cli/commands/info.rs
+++ tests/cli/commands/info.rs
@@ -9,16 +9,16 @@ fn info() -> Result<(), Box<dyn std::error::Error>> {
     let arg_path = tempfile.path();
 
     let out_str = "\
-Discipline:                             Meteorological products
-Total Length:                           10321
-Originating/generating centre:          Tokyo (RSMC), Japan Meteorological Agency
-Originating/generating sub-centre:      0
-GRIB Master Tables Version Number:      5 (4 November 2009)
-GRIB Local Tables Version Number:       1 (Number of local tables version used)
-Significance of Reference Time:         Analysis
-Reference time of data:                 2016-08-22 02:00:00 UTC
-Production status of processed data:    Operational products
-Type of processed data:                 Analysis and forecast products
+    Discipline:                             Meteorological products
+    Total Length:                           10321
+    Originating/generating centre:          Tokyo (RSMC), Japan Meteorological Agency
+    Originating/generating sub-centre:      0
+    GRIB Master Tables Version Number:      5 (4 November 2009)
+    GRIB Local Tables Version Number:       1 (Number of local tables version used)
+    Significance of Reference Time:         Analysis
+    Reference time of data:                 2016-08-22 02:00:00 UTC
+    Production status of processed data:    Operational products
+    Type of processed data:                 Analysis and forecast products
 ";
 
     let mut cmd = Command::cargo_bin(CMD_NAME)?;
