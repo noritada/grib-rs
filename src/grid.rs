a852a4b4402c0f3275fb3ca181216437da3641fd
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2023-02-02 21:32:17 +0900

    Implement the lat/lon grid definition

--- src/grid.rs
+++ src/grid.rs
@@ -1,3 +1,67 @@
+use crate::error::GribError;
+
+pub(crate) struct LatLonGridDefinition {
+    ni: u32,
+    nj: u32,
+    first_point_lat: i32,
+    first_point_lon: i32,
+    last_point_lat: i32,
+    last_point_lon: i32,
+    scanning_mode: ScanningMode,
+}
+
+impl LatLonGridDefinition {
+    pub(crate) fn new(
+        ni: u32,
+        nj: u32,
+        first_point_lat: i32,
+        first_point_lon: i32,
+        last_point_lat: i32,
+        last_point_lon: i32,
+        scanning_mode: ScanningMode,
+    ) -> Self {
+        Self {
+            ni,
+            nj,
+            first_point_lat,
+            first_point_lon,
+            last_point_lat,
+            last_point_lon,
+            scanning_mode,
+        }
+    }
+
+    pub(crate) fn iter(&self) -> Result<LatLonGridIterator, GribError> {
+        if !self.is_consistent() {
+            return Err(GribError::InvalidValueError("Latitude and longitude for first/last grid points are not consistent with scanning mode".to_owned()));
+        }
+
+        let lat_diff = self.last_point_lat - self.first_point_lat;
+        let lon_diff = self.last_point_lon - self.first_point_lon;
+        let (num_div_lat, num_div_lon) = ((self.nj - 1) as i32, (self.ni - 1) as i32);
+        let lat_delta = lat_diff as f32 / num_div_lat as f32;
+        let lat = (0..=num_div_lat)
+            .into_iter()
+            .map(|x| (self.first_point_lat as f32 + x as f32 * lat_delta) / 1_000_000 as f32)
+            .collect();
+        let lon_delta = lon_diff as f32 / num_div_lon as f32;
+        let lon = (0..=num_div_lon)
+            .into_iter()
+            .map(|x| (self.first_point_lon as f32 + x as f32 * lon_delta) / 1_000_000 as f32)
+            .collect();
+
+        let iter = LatLonGridIterator::new(lat, lon, self.scanning_mode);
+        Ok(iter)
+    }
+
+    fn is_consistent(&self) -> bool {
+        let lat_diff = self.last_point_lat - self.first_point_lat;
+        let lon_diff = self.last_point_lon - self.first_point_lon;
+        !(((lat_diff > 0) ^ self.scanning_mode.scans_positively_for_j())
+            || ((lon_diff > 0) ^ self.scanning_mode.scans_positively_for_i()))
+    }
+}
+
 pub(crate) struct LatLonGridIterator {
     major: Vec<f32>,
     minor: Vec<f32>,
@@ -64,6 +128,7 @@ impl Iterator for LatLonGridIterator {
     }
 }
 
+#[derive(Clone, Copy)]
 pub(crate) struct ScanningMode(u8);
 
 impl ScanningMode {
@@ -91,6 +156,130 @@ impl ScanningMode {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_lat_lon_grid_definition_and_iteration() {
+        let grid = LatLonGridDefinition::new(
+            3,
+            2,
+            36_000_000,
+            135_000_000,
+            35_000_000,
+            137_000_000,
+            ScanningMode(0b00000000),
+        );
+        let actual = grid.iter().unwrap().collect::<Vec<_>>();
+        let expected = vec![
+            (36., 135.),
+            (36., 136.),
+            (36., 137.),
+            (35., 135.),
+            (35., 136.),
+            (35., 137.),
+        ];
+        assert_eq!(actual, expected)
+    }
+
+    macro_rules! test_consistencies_between_lat_lon_and_scanning_mode {
+        ($((
+            $name:ident,
+            $first_point_lat:expr,
+            $first_point_lon:expr,
+            $last_point_lat:expr,
+            $last_point_lon:expr,
+            $scanning_mode:expr,
+            $expected:expr
+        ),)*) => ($(
+            #[test]
+            fn $name() {
+                let grid = LatLonGridDefinition::new(
+                    1,
+                    1,
+                    $first_point_lat,
+                    $first_point_lon,
+                    $last_point_lat,
+                    $last_point_lon,
+                    ScanningMode($scanning_mode),
+                );
+                assert_eq!(grid.is_consistent(), $expected)
+            }
+        )*);
+    }
+
+    test_consistencies_between_lat_lon_and_scanning_mode! {
+        (
+            consistency_between_lat_decrease_and_scanning_mode_0b00000000,
+            37_000_000,
+            140_000_000,
+            36_000_000,
+            141_000_000,
+            0b00000000,
+            true
+        ),
+        (
+            consistency_between_lat_decrease_and_scanning_mode_0b01000000,
+            37_000_000,
+            140_000_000,
+            36_000_000,
+            141_000_000,
+            0b01000000,
+            false
+        ),
+        (
+            consistency_between_lat_increase_and_scanning_mode_0b00000000,
+            36_000_000,
+            140_000_000,
+            37_000_000,
+            141_000_000,
+            0b00000000,
+            false
+        ),
+        (
+            consistency_between_lat_increase_and_scanning_mode_0b01000000,
+            36_000_000,
+            140_000_000,
+            37_000_000,
+            141_000_000,
+            0b01000000,
+            true
+        ),
+        (
+            consistency_between_lon_increase_and_scanning_mode_0b00000000,
+            37_000_000,
+            140_000_000,
+            36_000_000,
+            141_000_000,
+            0b00000000,
+            true
+        ),
+        (
+            consistency_between_lon_increase_and_scanning_mode_0b10000000,
+            37_000_000,
+            140_000_000,
+            36_000_000,
+            141_000_000,
+            0b10000000,
+            false
+        ),
+        (
+            consistency_between_lon_decrease_and_scanning_mode_0b00000000,
+            37_000_000,
+            141_000_000,
+            36_000_000,
+            140_000_000,
+            0b00000000,
+            false
+        ),
+        (
+            consistency_between_lon_decrease_and_scanning_mode_0b10000000,
+            37_000_000,
+            141_000_000,
+            36_000_000,
+            140_000_000,
+            0b10000000,
+            true
+        ),
+    }
 
     macro_rules! test_lat_lon_grid_iter {
         ($(($name:ident, $scanning_mode:expr, $expected:expr),)*) => ($(
