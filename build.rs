32c3ddd108ac75d9553107149113505ff31b686c
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2023-10-01 15:54:30 +0900

    build: improve error handling in build.rs

--- build.rs
+++ build.rs
@@ -3,23 +3,23 @@ use std::{
     path::{Path, PathBuf},
 };
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = env::var_os("OUT_DIR").unwrap();
 
     if let Err(e) = check_nonemptiness(&PathBuf::from("def/CCT"))
         .and(check_nonemptiness(&PathBuf::from("def/GRIB2")))
     {
-        panic!("error: {e}; run `git submodule update --init`")
+        return Err(format!("error: {}; run `git submodule update --init`", e).into());
     }
 
     let input_file_names = ["def/CCT/C00.csv", "def/CCT/C11.csv"];
     let mut db = grib_build::cct_csv::CodeDB::new();
     let output_path = Path::new(&out_dir).join("cct.rs");
     for path in &input_file_names {
-        db.load(path).unwrap();
+        db.load(path)?;
         println!("cargo:rerun-if-changed={path}");
     }
-    fs::write(output_path, format!("{db}")).unwrap();
+    fs::write(output_path, format!("{db}"))?;
 
     let input_file_names = [
         "def/GRIB2/GRIB2_CodeFlag_0_0_CodeTable_en.csv",
@@ -39,12 +39,13 @@ fn main() {
     let mut db = grib_build::grib2_codeflag_csv::CodeDB::new();
     let output_path = Path::new(&out_dir).join("grib2_codeflag.rs");
     for path in &input_file_names {
-        db.load(path).unwrap();
+        db.load(path)?;
         println!("cargo:rerun-if-changed={path}");
     }
-    fs::write(output_path, format!("{db}")).unwrap();
+    fs::write(output_path, format!("{db}"))?;
 
     println!("cargo:rerun-if-changed=build.rs");
+    Ok(())
 }
 
 fn check_nonemptiness(dir: &PathBuf) -> Result<(), String> {
