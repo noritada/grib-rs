e9d12b4fa224bc08d08d9b021c41bb424bd5cce1
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-01-21 00:33:29 +0900

    Comment on the origin of the values used for testing

--- src/grid/lambert.rs
+++ src/grid/lambert.rs
@@ -251,6 +251,8 @@ mod tests {
             latin2: 25000000,
         };
         let latlons = grid_def.latlons()?.collect::<Vec<_>>();
+
+        // Following lat/lon values are taken from the calculation results using pygrib.
         let delta = 1e-10;
         assert_coord_almost_eq(latlons[0], (20.19, -121.550004), delta);
         assert_coord_almost_eq(latlons[1], (20.19442682, -121.52621665), delta);
