c220a232783a6feb5af180a5b822e99bef6f5544
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-07-03 01:09:01 +0900

    Absorb differences in text on Windows to prevent a test failure

--- cli/tests/cli/main.rs
+++ cli/tests/cli/main.rs
@@ -30,8 +30,12 @@ fn readme_consistent_with_help_message() -> Result<(), Box<dyn std::error::Error
     cmd.arg("--help");
     let help_msg = cmd.output()?.stdout;
     let help_msg = String::from_utf8(help_msg)?;
+    #[cfg(target_os = "windows")]
+    let help_msg = help_msg.replace(&format!("{}.exe", CMD_NAME), CMD_NAME);
 
     let readme = include_str!("../../../README.md");
+    #[cfg(target_os = "windows")]
+    let readme = readme.replace("\r\n", "\n");
     assert!(readme.contains(&help_msg));
 
     Ok(())
