2424d9f09ec7b7eacf6f7b7554adf18761a94405
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-09-06 23:57:10 +0900

    macros: import the initial version

---
+++ macros/src/lib.rs
@@ -0,0 +1,1 @@
+mod wgrib2;
---
+++ macros/src/wgrib2.rs
@@ -0,0 +1,123 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Wgrib2Table(Vec<Wgrib2TableEntry>);
+
+impl Wgrib2Table {
+    fn from_file<P>(path: P) -> Result<Self, &'static str>
+    where
+        P: AsRef<std::path::Path>,
+    {
+        let s = std::fs::read_to_string(path).map_err(|_| "reading file failed")?;
+        s.parse::<Self>()
+    }
+
+    fn from_str_impl(s: &str) -> Option<Self> {
+        let lines: Option<Vec<_>> = s
+            .lines()
+            .map(|line| line.trim_end().parse::<Wgrib2TableEntry>().ok())
+            .collect();
+        Some(Self(lines?))
+    }
+}
+
+impl FromStr for Wgrib2Table {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::from_str_impl(s).ok_or("parsing as a wgrib2 table failed")
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Wgrib2TableEntry {
+    discipline: u8,
+    master_table_start_ver: u8,
+    master_table_end_ver: u8,
+    centre: u16,
+    local_table_ver: u8,
+    param_category: u8,
+    param_number: u8,
+    name: String,
+    desc: String,
+    unit: String,
+}
+
+impl Wgrib2TableEntry {
+    fn from_str_impl(s: &str) -> Option<Self> {
+        let mut words = s.split(":");
+        let discipline = words.next()?.parse::<u8>().ok()?;
+        let _ = words.next()?;
+        let master_table_start_ver = words.next()?.parse::<u8>().ok()?;
+        let master_table_end_ver = words.next()?.parse::<u8>().ok()?;
+        let centre = words.next()?.parse::<u16>().ok()?;
+        let local_table_ver = words.next()?.parse::<u8>().ok()?;
+        let param_category = words.next()?.parse::<u8>().ok()?;
+        let param_number = words.next()?.parse::<u8>().ok()?;
+        let name = words.next()?.to_owned();
+        let desc = words.next()?.to_owned();
+        let unit = words.next()?.to_owned();
+        Some(Self {
+            discipline,
+            master_table_start_ver,
+            master_table_end_ver,
+            centre,
+            local_table_ver,
+            param_category,
+            param_number,
+            name,
+            desc,
+            unit,
+        })
+    }
+}
+
+impl FromStr for Wgrib2TableEntry {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::from_str_impl(s).ok_or("parsing as a wgrib2 table failed")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsing_wgrib2_table() {
+        let input = "\
+0:1:0:255:0:0:0:0:TMP:Temperature:K
+0:1:0:255:0:0:0:1:VTMP:Virtual Temperature:K
+";
+        let actual = input.parse::<Wgrib2Table>();
+        let expected = vec![
+            Wgrib2TableEntry {
+                discipline: 0,
+                master_table_start_ver: 0,
+                master_table_end_ver: 255,
+                centre: 0,
+                local_table_ver: 0,
+                param_category: 0,
+                param_number: 0,
+                name: "TMP".to_owned(),
+                desc: "Temperature".to_owned(),
+                unit: "K".to_owned(),
+            },
+            Wgrib2TableEntry {
+                discipline: 0,
+                master_table_start_ver: 0,
+                master_table_end_ver: 255,
+                centre: 0,
+                local_table_ver: 0,
+                param_category: 0,
+                param_number: 1,
+                name: "VTMP".to_owned(),
+                desc: "Virtual Temperature".to_owned(),
+                unit: "K".to_owned(),
+            },
+        ];
+        let expected = Ok(Wgrib2Table(expected));
+        assert_eq!(actual, expected);
+    }
+}
