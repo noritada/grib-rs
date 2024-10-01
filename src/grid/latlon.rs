fbfd540fdaee8bb2240ad486580f748863102fbd
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-10-02 02:49:42 +0900

    Fix longitude computation failures due to inconsistent grid definitions
    Closes #51.

--- src/grid/latlon.rs
+++ src/grid/latlon.rs
@@ -111,8 +111,11 @@ impl LatLonGridDefinition {
     /// assert_eq!(latlons.next(), Some((1.0, 0.0)));
     /// ```
     pub fn latlons(&self) -> Result<RegularGridIterator, GribError> {
-        if !self.is_consistent() {
-            return Err(GribError::InvalidValueError("Latitude and longitude for first/last grid points are not consistent with scanning mode".to_owned()));
+        if !self.is_consistent_for_j() {
+            return Err(GribError::InvalidValueError(
+                "Latitudes for first/last grid points are not consistent with scanning mode"
+                    .to_owned(),
+            ));
         }
 
         let ij = self.ij()?;
@@ -121,21 +124,43 @@ impl LatLonGridDefinition {
             self.last_point_lat as f32,
             (self.nj - 1) as usize,
         );
-        let lon = evenly_spaced_degrees(
-            self.first_point_lon as f32,
-            self.last_point_lon as f32,
-            (self.ni - 1) as usize,
-        );
+        let lon = self.lons();
 
         let iter = RegularGridIterator::new(lat, lon, ij);
         Ok(iter)
     }
 
-    pub(crate) fn is_consistent(&self) -> bool {
-        let lat_diff = self.last_point_lat - self.first_point_lat;
+    fn lons(&self) -> Vec<f32> {
+        let (first_lon, last_lon) = (self.first_point_lon as f32, self.last_point_lon as f32);
+        let (first_lon, last_lon) = if self.is_consistent_for_i() {
+            (first_lon, last_lon)
+        } else {
+            if self.first_point_lon > self.last_point_lon {
+                (first_lon, last_lon + 360_000_000_f32)
+            } else {
+                (first_lon + 360_000_000_f32, last_lon)
+            }
+        };
+
+        let lon = evenly_spaced_degrees(first_lon, last_lon, (self.ni - 1) as usize);
+
+        if self.is_consistent_for_i() {
+            lon
+        } else {
+            lon.into_iter()
+                .map(|x| if x < 360.0 { x } else { x - 360.0 })
+                .collect::<Vec<_>>()
+        }
+    }
+
+    pub(crate) fn is_consistent_for_i(&self) -> bool {
         let lon_diff = self.last_point_lon - self.first_point_lon;
-        !(((lat_diff > 0) ^ self.scanning_mode.scans_positively_for_j())
-            || ((lon_diff > 0) ^ self.scanning_mode.scans_positively_for_i()))
+        !((lon_diff > 0) ^ self.scanning_mode.scans_positively_for_i())
+    }
+
+    pub(crate) fn is_consistent_for_j(&self) -> bool {
+        let lat_diff = self.last_point_lat - self.first_point_lat;
+        !((lat_diff > 0) ^ self.scanning_mode.scans_positively_for_j())
     }
 
     pub(crate) fn from_buf(buf: &[u8]) -> Self {
@@ -252,7 +277,8 @@ mod tests {
             $last_point_lat:expr,
             $last_point_lon:expr,
             $scanning_mode:expr,
-            $expected:expr
+            $expected_for_i:expr,
+            $expected_for_j:expr
         ),)*) => ($(
             #[test]
             fn $name() {
@@ -265,7 +291,8 @@ mod tests {
                     last_point_lon: $last_point_lon,
                     scanning_mode: ScanningMode($scanning_mode),
                 };
-                assert_eq!(grid.is_consistent(), $expected)
+                assert_eq!(grid.is_consistent_for_i(), $expected_for_i);
+                assert_eq!(grid.is_consistent_for_j(), $expected_for_j);
             }
         )*);
     }
@@ -278,6 +305,7 @@ mod tests {
             36_000_000,
             141_000_000,
             0b00000000,
+            true,
             true
         ),
         (
@@ -287,6 +315,7 @@ mod tests {
             36_000_000,
             141_000_000,
             0b01000000,
+            true,
             false
         ),
         (
@@ -296,6 +325,7 @@ mod tests {
             37_000_000,
             141_000_000,
             0b00000000,
+            true,
             false
         ),
         (
@@ -305,6 +335,7 @@ mod tests {
             37_000_000,
             141_000_000,
             0b01000000,
+            true,
             true
         ),
         (
@@ -314,6 +345,7 @@ mod tests {
             36_000_000,
             141_000_000,
             0b00000000,
+            true,
             true
         ),
         (
@@ -323,7 +355,8 @@ mod tests {
             36_000_000,
             141_000_000,
             0b10000000,
-            false
+            false,
+            true
         ),
         (
             consistency_between_lon_decrease_and_scanning_mode_0b00000000,
@@ -332,7 +365,8 @@ mod tests {
             36_000_000,
             140_000_000,
             0b00000000,
-            false
+            false,
+            true
         ),
         (
             consistency_between_lon_decrease_and_scanning_mode_0b10000000,
@@ -341,6 +375,7 @@ mod tests {
             36_000_000,
             140_000_000,
             0b10000000,
+            true,
             true
         ),
     }
