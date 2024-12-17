b7c5078bb14ca6f7449c6b689b103b6b0a9e2758
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-12-17 20:02:51 +0900

    grib-template-derive: add a test for nested structs

--- grib-template-derive/tests/reading.rs
+++ grib-template-derive/tests/reading.rs
@@ -10,16 +10,32 @@ pub struct Params {
     field3: u16,
     /// Field 4
     field4: u32,
+    /// Field 5
+    field5: InnerParams,
+}
+
+#[derive(Debug, PartialEq, Eq, grib_template_derive::FromSlice)]
+pub struct InnerParams {
+    /// Field 1
+    field1: u8,
+    /// Field 2
+    field2: u8,
 }
 
 fn main() {
-    let buf = vec![0x01_u8, 0xff, 0x00, 0x01, 0x23, 0x76, 0x54, 0x32, 0x10];
+    let buf = vec![
+        0x01_u8, 0xff, 0x00, 0x01, 0x23, 0x76, 0x54, 0x32, 0x10, 0xf0, 0x0f,
+    ];
     let actual = Params::from_slice(&buf);
     let expected = Params {
         field1: 0x01,
         field2: 0xff00,
         field3: 0x0123,
         field4: 0x76543210,
+        field5: InnerParams {
+            field1: 0xf0,
+            field2: 0x0f,
+        },
     };
 
     assert_eq!(actual, expected)
