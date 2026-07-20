56c4274b857388742261d48cc8ad67d2f9cdfb53
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2026-07-20 15:50:16 +0900

    Represent test data as constants rather than functions
    When developing with the help of LSP, since information about the
    contents of functions is not available, representing test data as
    functions requires an extra step of looking up the function definition
    to determine which data file the function name refers to.
    
    On the other hand, representing test data as constants allows LSP to
    return their values (the paths to the data files), and since these
    values are displayed in pop-ups or similar features in an IDE, for
    example, the paths to the test data files can be obtained directly
    from the identifiers.

--- src/decoder/tests.rs
+++ src/decoder/tests.rs
@@ -35,41 +35,41 @@ macro_rules! test_operation_with_data_without_nan_values {
 test_operation_with_data_without_nan_values! {
     (
         decoding_simple_packing,
-        test_utils::data::grib2::jma_kousa(),
+        test_utils::data::grib2::JMA_KOUSA,
         (0, 3),
-        test_utils::data::flat_binary::jma_kousa_le()
+        test_utils::data::flat_binary::JMA_KOUSA_LE
     ),
     (
         decoding_complex_packing_with_num_descriptor_octet_being_1,
-        test_utils::data::grib2::noaa_gdas_12(),
+        test_utils::data::grib2::NOAA_GDAS_12,
         (0, 0),
-        test_utils::data::flat_binary::noaa_gdas_12_le()
+        test_utils::data::flat_binary::NOAA_GDAS_12_LE
     ),
     (
         decoding_complex_packing_with_num_descriptor_octet_being_2,
-        test_utils::data::grib2::jma_meps(),
+        test_utils::data::grib2::JMA_MEPS,
         (0, 2),
-        test_utils::data::flat_binary::jma_meps_le()
+        test_utils::data::flat_binary::JMA_MEPS_LE
     ),
     (
         decoding_complex_packing_where_nbit_is_zero,
-        test_utils::data::grib2::noaa_gdas_46(),
+        test_utils::data::grib2::NOAA_GDAS_46,
         (0, 0),
-        test_utils::data::flat_binary::noaa_gdas_46_le()
+        test_utils::data::flat_binary::NOAA_GDAS_46_LE
     ),
     #[cfg(feature = "png-unpack-with-png-crate")]
     (
         decoding_png_packing_with_num_bits_being_8,
-        test_utils::data::grib2::noaa_mrms_precip_flag(),
+        test_utils::data::grib2::NOAA_MRMS_PRECIP_FLAG,
         (0, 0),
-        test_utils::data::flat_binary::noaa_mrms_precip_flag_le()
+        test_utils::data::flat_binary::NOAA_MRMS_PRECIP_FLAG_LE
     ),
     #[cfg(feature = "png-unpack-with-png-crate")]
     (
         decoding_png_packing_with_num_bits_being_16,
-        test_utils::data::grib2::noaa_mrms_reflectivity(),
+        test_utils::data::grib2::NOAA_MRMS_REFLECTIVITY,
         (0, 0),
-        test_utils::data::flat_binary::noaa_mrms_reflectivity_le()
+        test_utils::data::flat_binary::NOAA_MRMS_REFLECTIVITY_LE
     ),
     #[cfg(any(
         feature = "ccsds-unpack-with-libaec",
@@ -77,9 +77,9 @@ test_operation_with_data_without_nan_values! {
     ))]
     (
         decoding_ccsds_compression,
-        test_utils::data::grib2::ecmwf_realtime_oper_fc_0(),
+        test_utils::data::grib2::ECMWF_REALTIME_OPER_FC_0,
         (0, 0),
-        test_utils::data::flat_binary::ecmwf_realtime_oper_fc_0_le()
+        test_utils::data::flat_binary::ECMWF_REALTIME_OPER_FC_0_LE
     ),
     #[cfg(any(
         feature = "ccsds-unpack-with-libaec",
@@ -87,9 +87,9 @@ test_operation_with_data_without_nan_values! {
     ))]
     (
         decoding_ccsds_compression_where_num_bits_is_multiple_of_eight,
-        test_utils::data::grib2::ecmwf_realtime_oper_fc_89(),
+        test_utils::data::grib2::ECMWF_REALTIME_OPER_FC_89,
         (0, 0),
-        test_utils::data::flat_binary::ecmwf_realtime_oper_fc_89_le()
+        test_utils::data::flat_binary::ECMWF_REALTIME_OPER_FC_89_LE
     ),
 }
 
@@ -137,39 +137,39 @@ macro_rules! test_operation_with_data_with_nan_values {
 test_operation_with_data_with_nan_values! {
     (
         decoding_run_length_packing,
-        test_utils::data::grib2::jma_tornado_nowcast(),
+        test_utils::data::grib2::JMA_TORNADO_NOWCAST,
         (0, 3),
-        test_utils::data::flat_binary::jma_tornado_nowcast_le()
+        test_utils::data::flat_binary::JMA_TORNADO_NOWCAST_LE
     ),
     (
         decoding_simple_packing_with_bitmap,
-        test_utils::data::grib2::jma_msmguid(),
+        test_utils::data::grib2::JMA_MSMGUID,
         (0, 0),
-        test_utils::data::flat_binary::jma_msmguid_le()
+        test_utils::data::flat_binary::JMA_MSMGUID_LE
     ),
     (
         decoding_complex_packing_with_first_order_spatial_differencing,
-        test_utils::data::grib2::ncmrwf_wind_solar(),
+        test_utils::data::grib2::NCMRWF_WIND_SOLAR,
         (0, 0),
-        test_utils::data::flat_binary::ncmrwf_wind_solar_le()
+        test_utils::data::flat_binary::NCMRWF_WIND_SOLAR_LE
     ),
     (
         decoding_complex_packing_without_spatial_differencing,
-        test_utils::data::grib2::noaa_ndfd_critfireo(),
+        test_utils::data::grib2::NOAA_NDFD_CRITFIREO,
         (0, 0),
-        test_utils::data::flat_binary::noaa_ndfd_critfireo_0_le()
+        test_utils::data::flat_binary::NOAA_NDFD_CRITFIREO_0_LE
     ),
     (
         decoding_complex_packing_without_spatial_differencing_when_nbit_is_zero,
-        test_utils::data::grib2::noaa_ndfd_critfireo(),
+        test_utils::data::grib2::NOAA_NDFD_CRITFIREO,
         (1, 0),
-        test_utils::data::flat_binary::noaa_ndfd_critfireo_1_le()
+        test_utils::data::flat_binary::NOAA_NDFD_CRITFIREO_1_LE
     ),
     (
         decoding_complex_packing_with_missing_value_management,
-        test_utils::data::grib2::noaa_ndfd_minrh(),
+        test_utils::data::grib2::NOAA_NDFD_MINRH,
         (0, 0),
-        test_utils::data::flat_binary::noaa_ndfd_minrh_0_le()
+        test_utils::data::flat_binary::NOAA_NDFD_MINRH_0_LE
     ),
 }
 
@@ -224,48 +224,48 @@ test_operation_with_data_without_nan_values_compared_using_simple_packing! {
     ))]
     (
         decoding_jpeg2000_code_stream,
-        test_utils::data::grib2::cmc_glb(),
+        test_utils::data::grib2::CMC_GLB,
         (0, 0),
         f32::from_be_bytes([0x45, 0x0e, 0xcc, 0x05]),
         -2,
         1,
-        test_utils::data::flat_binary::cmc_glb_le()
+        test_utils::data::flat_binary::CMC_GLB_LE
     ),
     (
         decoding_complex_packing_with_num_descriptor_octet_being_3,
-        test_utils::data::grib2::noaa_gdas_0_10(),
+        test_utils::data::grib2::NOAA_GDAS_0_10,
         (1, 0),
         f32::from_be_bytes([0x00, 0x00, 0x00, 0x00]),
         1,
         8,
-        test_utils::data::flat_binary::noaa_gdas_1_le()
+        test_utils::data::flat_binary::NOAA_GDAS_1_LE
     ),
     (
         decoding_complex_packing_with_3_byte_spatial_differencing_extra_descriptors_starting_from_0x80,
-        test_utils::data::grib2::noaa_gdas_0_10(),
+        test_utils::data::grib2::NOAA_GDAS_0_10,
         (0, 0),
         f32::from_be_bytes([0x49, 0x67, 0xe7, 0xdf]),
         1,
         1,
-        test_utils::data::flat_binary::noaa_gdas_0_le()
+        test_utils::data::flat_binary::NOAA_GDAS_0_LE
     ),
     (
         decoding_complex_packing_with_zero_width_groups,
-        test_utils::data::grib2::noaa_gdas_0_10(),
+        test_utils::data::grib2::NOAA_GDAS_0_10,
         (2, 0),
         f32::from_be_bytes([0x00, 0x00, 0x00, 0x00]),
         3,
         9,
-        test_utils::data::flat_binary::noaa_gdas_2_le()
+        test_utils::data::flat_binary::NOAA_GDAS_2_LE
     ),
     #[cfg(feature = "png-unpack-with-png-crate")]
     (
         decoding_png_packing_with_num_bits_being_24,
-        test_utils::data::grib2::noaa_mrms_merged_rho_hv(),
+        test_utils::data::grib2::NOAA_MRMS_MERGED_RHO_HV,
         (0, 0),
         f32::from_be_bytes([0xc7, 0xc3, 0x1e, 0x00]),
         0,
         2,
-        test_utils::data::flat_binary::noaa_mrms_merged_rho_hv_le()
+        test_utils::data::flat_binary::NOAA_MRMS_MERGED_RHO_HV_LE
     ),
 }
--- src/test_utils/data.rs
+++ src/test_utils/data.rs
@@ -1,59 +1,57 @@
 macro_rules! definitions_of_test_data {
     ($(($name:ident, $file_name:expr),)*) => ($(
         #[allow(dead_code)]
-        pub(crate) fn $name() -> &'static str {
-            $file_name
-        }
+        pub(crate) const $name: &'static str = $file_name;
     )*);
 }
 
 pub(crate) mod grib2 {
     definitions_of_test_data! {
-        (cmc_glb, "testdata/CMC_glb_TMP_ISBL_1_latlon.24x.24_2021051800_P000.grib2"),
-        (ecmwf_realtime_oper_fc_0, "testdata/20240101000000-0h-oper-fc.grib2.0-10.xz"),
-        (ecmwf_realtime_oper_fc_89, "testdata/20250912120000-0h-oper-fc.grib2.89.xz"),
+        (CMC_GLB, "testdata/CMC_glb_TMP_ISBL_1_latlon.24x.24_2021051800_P000.grib2"),
+        (ECMWF_REALTIME_OPER_FC_0, "testdata/20240101000000-0h-oper-fc.grib2.0-10.xz"),
+        (ECMWF_REALTIME_OPER_FC_89, "testdata/20250912120000-0h-oper-fc.grib2.89.xz"),
         (
-            jma_kousa,
+            JMA_KOUSA,
             "testdata/Z__C_RJTD_20170221120000_MSG_GPV_Gll0p5deg_Pys_B20170221120000_F2017022115-2017022212_grib2.bin.xz"
         ),
-        (jma_meps, "testdata/Z__C_RJTD_20190605000000_MEPS_GPV_Rjp_L-pall_FH00-15_grib2.bin.0-20.xz"),
-        (jma_msmguid, "testdata/Z__C_RJTD_20190304000000_MSM_GUID_Rjp_P-all_FH03-39_Toorg_grib2.bin.xz"),
+        (JMA_MEPS, "testdata/Z__C_RJTD_20190605000000_MEPS_GPV_Rjp_L-pall_FH00-15_grib2.bin.0-20.xz"),
+        (JMA_MSMGUID, "testdata/Z__C_RJTD_20190304000000_MSM_GUID_Rjp_P-all_FH03-39_Toorg_grib2.bin.xz"),
         (
-            jma_tornado_nowcast,
+            JMA_TORNADO_NOWCAST,
             "testdata/Z__C_RJTD_20160822020000_NOWC_GPV_Ggis10km_Pphw10_FH0000-0100_grib2.bin"
         ),
-        (ncmrwf_wind_solar, "testdata/wind_solar_ind_0.125_20240521_12Z.grib2.0.xz"),
-        (noaa_gdas_0_10, "testdata/gdas.t12z.pgrb2.0p25.f000.0-10.xz"),
-        (noaa_gdas_12, "testdata/gdas.t12z.pgrb2.0p25.f000.12.xz"),
-        (noaa_gdas_46, "testdata/gdas.t12z.pgrb2.0p25.f000.46.xz"),
-        (noaa_mrms_merged_rho_hv, "testdata/MRMS_MergedRhoHV_19.00_20260219-042039.grib2.gz"),
-        (noaa_mrms_precip_flag, "testdata/MRMS_PrecipFlag_00.00_20260219-042400.grib2.gz"),
-        (noaa_mrms_reflectivity, "testdata/MRMS_ReflectivityAtLowestAltitude_00.50_20230406-120039.grib2.gz"),
-        (noaa_ndfd_critfireo, "testdata/ds.critfireo.bin.xz"),
-        (noaa_ndfd_minrh, "testdata/ds.minrh.bin.xz"),
+        (NCMRWF_WIND_SOLAR, "testdata/wind_solar_ind_0.125_20240521_12Z.grib2.0.xz"),
+        (NOAA_GDAS_0_10, "testdata/gdas.t12z.pgrb2.0p25.f000.0-10.xz"),
+        (NOAA_GDAS_12, "testdata/gdas.t12z.pgrb2.0p25.f000.12.xz"),
+        (NOAA_GDAS_46, "testdata/gdas.t12z.pgrb2.0p25.f000.46.xz"),
+        (NOAA_MRMS_MERGED_RHO_HV, "testdata/MRMS_MergedRhoHV_19.00_20260219-042039.grib2.gz"),
+        (NOAA_MRMS_PRECIP_FLAG, "testdata/MRMS_PrecipFlag_00.00_20260219-042400.grib2.gz"),
+        (NOAA_MRMS_REFLECTIVITY, "testdata/MRMS_ReflectivityAtLowestAltitude_00.50_20230406-120039.grib2.gz"),
+        (NOAA_NDFD_CRITFIREO, "testdata/ds.critfireo.bin.xz"),
+        (NOAA_NDFD_MINRH, "testdata/ds.minrh.bin.xz"),
     }
 }
 
 pub(crate) mod flat_binary {
     definitions_of_test_data! {
-        (cmc_glb_le, "testdata/gen/cmc-glb-wgrib2-le.bin.xz"),
-        (ecmwf_realtime_oper_fc_0_le, "testdata/gen/ecmwf-realtime-oper-fc-0-le.bin.xz"),
-        (ecmwf_realtime_oper_fc_89_le, "testdata/gen/ecmwf-realtime-oper-fc-89-le.bin.xz"),
-        (jma_kousa_le, "testdata/gen/kousa-wgrib2-le.bin.xz"),
-        (jma_meps_le, "testdata/gen/meps-wgrib2-le.bin.xz"),
-        (jma_msmguid_le, "testdata/gen/msmguid-wgrib2-le.bin.xz"),
-        (jma_tornado_nowcast_le, "testdata/gen/tornado-wgrib2-le.bin.xz"),
-        (ncmrwf_wind_solar_le, "testdata/gen/wind_solar_ind_0.125_20240521_12Z.wgrib2-le.bin.xz"),
-        (noaa_gdas_0_le, "testdata/gen/gdas-0-wgrib2-le.bin.xz"),
-        (noaa_gdas_1_le, "testdata/gen/gdas-1-wgrib2-le.bin.xz"),
-        (noaa_gdas_2_le, "testdata/gen/gdas-2-wgrib2-le.bin.xz"),
-        (noaa_gdas_12_le, "testdata/gen/gdas-12-wgrib2-le.bin.xz"),
-        (noaa_gdas_46_le, "testdata/gen/gdas-46-wgrib2-le.bin.xz"),
-        (noaa_mrms_merged_rho_hv_le, "testdata/gen/mrms-merged-rho-hv-wgrib2-le.bin.xz"),
-        (noaa_mrms_precip_flag_le, "testdata/gen/mrms-precip-flag-wgrib2-le.bin.xz"),
-        (noaa_mrms_reflectivity_le, "testdata/gen/mrms-reflectivity-wgrib2-le.bin.xz"),
-        (noaa_ndfd_critfireo_0_le, "testdata/gen/ds.critfireo.bin.0.xz"),
-        (noaa_ndfd_critfireo_1_le, "testdata/gen/ds.critfireo.bin.1.xz"),
-        (noaa_ndfd_minrh_0_le, "testdata/gen/ds.minrh.bin.0.xz"),
+        (CMC_GLB_LE, "testdata/gen/cmc-glb-wgrib2-le.bin.xz"),
+        (ECMWF_REALTIME_OPER_FC_0_LE, "testdata/gen/ecmwf-realtime-oper-fc-0-le.bin.xz"),
+        (ECMWF_REALTIME_OPER_FC_89_LE, "testdata/gen/ecmwf-realtime-oper-fc-89-le.bin.xz"),
+        (JMA_KOUSA_LE, "testdata/gen/kousa-wgrib2-le.bin.xz"),
+        (JMA_MEPS_LE, "testdata/gen/meps-wgrib2-le.bin.xz"),
+        (JMA_MSMGUID_LE, "testdata/gen/msmguid-wgrib2-le.bin.xz"),
+        (JMA_TORNADO_NOWCAST_LE, "testdata/gen/tornado-wgrib2-le.bin.xz"),
+        (NCMRWF_WIND_SOLAR_LE, "testdata/gen/wind_solar_ind_0.125_20240521_12Z.wgrib2-le.bin.xz"),
+        (NOAA_GDAS_0_LE, "testdata/gen/gdas-0-wgrib2-le.bin.xz"),
+        (NOAA_GDAS_1_LE, "testdata/gen/gdas-1-wgrib2-le.bin.xz"),
+        (NOAA_GDAS_2_LE, "testdata/gen/gdas-2-wgrib2-le.bin.xz"),
+        (NOAA_GDAS_12_LE, "testdata/gen/gdas-12-wgrib2-le.bin.xz"),
+        (NOAA_GDAS_46_LE, "testdata/gen/gdas-46-wgrib2-le.bin.xz"),
+        (NOAA_MRMS_MERGED_RHO_HV_LE, "testdata/gen/mrms-merged-rho-hv-wgrib2-le.bin.xz"),
+        (NOAA_MRMS_PRECIP_FLAG_LE, "testdata/gen/mrms-precip-flag-wgrib2-le.bin.xz"),
+        (NOAA_MRMS_REFLECTIVITY_LE, "testdata/gen/mrms-reflectivity-wgrib2-le.bin.xz"),
+        (NOAA_NDFD_CRITFIREO_0_LE, "testdata/gen/ds.critfireo.bin.0.xz"),
+        (NOAA_NDFD_CRITFIREO_1_LE, "testdata/gen/ds.critfireo.bin.1.xz"),
+        (NOAA_NDFD_MINRH_0_LE, "testdata/gen/ds.minrh.bin.0.xz"),
     }
 }
