4d731dcc89396e8a6dcaad63ee92063fa0eaf68a
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2026-07-20 15:06:14 +0900

    Use test data file paths as input parameters rather than their content data in parameterized tests
    Using the decompressed data contained in test data files as input is
    preferable based on the DRY principle. However, when we want to
    temporarily add test cases that use local data files not included in the
    repository, it is easier to work with the path as a parameter.
    
    However, if a user discovers an
    issue and wants to temporarily add a test case that uses a local data
    file as input, it is easier to work with if the path is specified as a
    parameter.
    
    Therefore, using data as a parameter is an excessive level of
    abstraction, and using paths as input is more user-friendly.

--- src/decoder/tests.rs
+++ src/decoder/tests.rs
@@ -10,7 +10,7 @@ macro_rules! test_operation_with_data_without_nan_values {
         $(#[$meta])*
         #[test]
         fn $name() -> Result<(), Box<dyn std::error::Error>> {
-            let buf = $input;
+            let buf = crate::test_utils::decompress_to_vec($input)?;
 
             let cursor = std::io::Cursor::new(buf);
             let grib2 = crate::from_reader(cursor)?;
@@ -21,7 +21,7 @@ macro_rules! test_operation_with_data_without_nan_values {
             let decoder = Grib2SubmessageDecoder::from(submessage)?;
             let actual = decoder.dispatch()?.collect::<Vec<_>>();
 
-            let expected = $expected
+            let expected = crate::test_utils::decompress_to_vec($expected)?
                 .chunks(4)
                 .map(|quad| f32::from_le_bytes(quad.try_into().unwrap())) // should be safely unwrapped
                 .collect::<Vec<_>>();
@@ -35,41 +35,41 @@ macro_rules! test_operation_with_data_without_nan_values {
 test_operation_with_data_without_nan_values! {
     (
         decoding_simple_packing,
-        test_utils::data::grib2::jma_kousa()?,
+        test_utils::data::grib2::jma_kousa(),
         (0, 3),
-        test_utils::data::flat_binary::jma_kousa_le()?
+        test_utils::data::flat_binary::jma_kousa_le()
     ),
     (
         decoding_complex_packing_with_num_descriptor_octet_being_1,
-        test_utils::data::grib2::noaa_gdas_12()?,
+        test_utils::data::grib2::noaa_gdas_12(),
         (0, 0),
-        test_utils::data::flat_binary::noaa_gdas_12_le()?
+        test_utils::data::flat_binary::noaa_gdas_12_le()
     ),
     (
         decoding_complex_packing_with_num_descriptor_octet_being_2,
-        test_utils::data::grib2::jma_meps()?,
+        test_utils::data::grib2::jma_meps(),
         (0, 2),
-        test_utils::data::flat_binary::jma_meps_le()?
+        test_utils::data::flat_binary::jma_meps_le()
     ),
     (
         decoding_complex_packing_where_nbit_is_zero,
-        test_utils::data::grib2::noaa_gdas_46()?,
+        test_utils::data::grib2::noaa_gdas_46(),
         (0, 0),
-        test_utils::data::flat_binary::noaa_gdas_46_le()?
+        test_utils::data::flat_binary::noaa_gdas_46_le()
     ),
     #[cfg(feature = "png-unpack-with-png-crate")]
     (
         decoding_png_packing_with_num_bits_being_8,
-        test_utils::data::grib2::noaa_mrms_precip_flag()?,
+        test_utils::data::grib2::noaa_mrms_precip_flag(),
         (0, 0),
-        test_utils::data::flat_binary::noaa_mrms_precip_flag_le()?
+        test_utils::data::flat_binary::noaa_mrms_precip_flag_le()
     ),
     #[cfg(feature = "png-unpack-with-png-crate")]
     (
         decoding_png_packing_with_num_bits_being_16,
-        test_utils::data::grib2::noaa_mrms_reflectivity()?,
+        test_utils::data::grib2::noaa_mrms_reflectivity(),
         (0, 0),
-        test_utils::data::flat_binary::noaa_mrms_reflectivity_le()?
+        test_utils::data::flat_binary::noaa_mrms_reflectivity_le()
     ),
     #[cfg(any(
         feature = "ccsds-unpack-with-libaec",
@@ -77,9 +77,9 @@ test_operation_with_data_without_nan_values! {
     ))]
     (
         decoding_ccsds_compression,
-        test_utils::data::grib2::ecmwf_realtime_oper_fc_0()?,
+        test_utils::data::grib2::ecmwf_realtime_oper_fc_0(),
         (0, 0),
-        test_utils::data::flat_binary::ecmwf_realtime_oper_fc_0_le()?
+        test_utils::data::flat_binary::ecmwf_realtime_oper_fc_0_le()
     ),
     #[cfg(any(
         feature = "ccsds-unpack-with-libaec",
@@ -87,9 +87,9 @@ test_operation_with_data_without_nan_values! {
     ))]
     (
         decoding_ccsds_compression_where_num_bits_is_multiple_of_eight,
-        test_utils::data::grib2::ecmwf_realtime_oper_fc_89()?,
+        test_utils::data::grib2::ecmwf_realtime_oper_fc_89(),
         (0, 0),
-        test_utils::data::flat_binary::ecmwf_realtime_oper_fc_89_le()?
+        test_utils::data::flat_binary::ecmwf_realtime_oper_fc_89_le()
     ),
 }
 
@@ -103,7 +103,7 @@ macro_rules! test_operation_with_data_with_nan_values {
         $(#[$meta])*
         #[test]
         fn $name() -> Result<(), Box<dyn std::error::Error>> {
-            let buf = $input;
+            let buf = crate::test_utils::decompress_to_vec($input)?;
 
             let cursor = std::io::Cursor::new(buf);
             let grib2 = crate::from_reader(cursor)?;
@@ -114,7 +114,7 @@ macro_rules! test_operation_with_data_with_nan_values {
             let decoder = Grib2SubmessageDecoder::from(submessage)?;
             let actual = decoder.dispatch()?.collect::<Vec<_>>();
 
-            let expected = $expected
+            let expected = crate::test_utils::decompress_to_vec($expected)?
                 .chunks(4)
                 .map(|b| match b {
                     [0x9a, 0xd1, 0x58, 0x62] => [0x00, 0x00, 0xc0, 0x7f],
@@ -137,39 +137,39 @@ macro_rules! test_operation_with_data_with_nan_values {
 test_operation_with_data_with_nan_values! {
     (
         decoding_run_length_packing,
-        test_utils::data::grib2::jma_tornado_nowcast()?,
+        test_utils::data::grib2::jma_tornado_nowcast(),
         (0, 3),
-        test_utils::data::flat_binary::jma_tornado_nowcast_le()?
+        test_utils::data::flat_binary::jma_tornado_nowcast_le()
     ),
     (
         decoding_simple_packing_with_bitmap,
-        test_utils::data::grib2::jma_msmguid()?,
+        test_utils::data::grib2::jma_msmguid(),
         (0, 0),
-        test_utils::data::flat_binary::jma_msmguid_le()?
+        test_utils::data::flat_binary::jma_msmguid_le()
     ),
     (
         decoding_complex_packing_with_first_order_spatial_differencing,
-        test_utils::data::grib2::ncmrwf_wind_solar()?,
+        test_utils::data::grib2::ncmrwf_wind_solar(),
         (0, 0),
-        test_utils::data::flat_binary::ncmrwf_wind_solar_le()?
+        test_utils::data::flat_binary::ncmrwf_wind_solar_le()
     ),
     (
         decoding_complex_packing_without_spatial_differencing,
-        test_utils::data::grib2::noaa_ndfd_critfireo()?,
+        test_utils::data::grib2::noaa_ndfd_critfireo(),
         (0, 0),
-        test_utils::data::flat_binary::noaa_ndfd_critfireo_0_le()?
+        test_utils::data::flat_binary::noaa_ndfd_critfireo_0_le()
     ),
     (
         decoding_complex_packing_without_spatial_differencing_when_nbit_is_zero,
-        test_utils::data::grib2::noaa_ndfd_critfireo()?,
+        test_utils::data::grib2::noaa_ndfd_critfireo(),
         (1, 0),
-        test_utils::data::flat_binary::noaa_ndfd_critfireo_1_le()?
+        test_utils::data::flat_binary::noaa_ndfd_critfireo_1_le()
     ),
     (
         decoding_complex_packing_with_missing_value_management,
-        test_utils::data::grib2::noaa_ndfd_minrh()?,
+        test_utils::data::grib2::noaa_ndfd_minrh(),
         (0, 0),
-        test_utils::data::flat_binary::noaa_ndfd_minrh_0_le()?
+        test_utils::data::flat_binary::noaa_ndfd_minrh_0_le()
     ),
 }
 
@@ -193,7 +193,7 @@ macro_rules! test_operation_with_data_without_nan_values_compared_using_simple_p
         $(#[$meta])*
         #[test]
         fn $name() -> Result<(), Box<dyn std::error::Error>> {
-            let buf = $input;
+            let buf = crate::test_utils::decompress_to_vec($input)?;
 
             let cursor = std::io::Cursor::new(buf);
             let grib2 = crate::from_reader(cursor)?;
@@ -207,7 +207,7 @@ macro_rules! test_operation_with_data_without_nan_values_compared_using_simple_p
             let ref_val = $ref_val;
             let exp: i16 = $exp;
             let dig: i16 = $dig;
-            let expected = $expected;
+            let expected = crate::test_utils::decompress_to_vec($expected)?;
             let expected = test_utils::encode_le_bytes_using_simple_packing(expected, ref_val, exp, dig);
             let actual = test_utils::encode_using_simple_packing(actual, ref_val, exp, dig);
             assert_eq!(actual, expected);
@@ -224,48 +224,48 @@ test_operation_with_data_without_nan_values_compared_using_simple_packing! {
     ))]
     (
         decoding_jpeg2000_code_stream,
-        test_utils::data::grib2::cmc_glb()?,
+        test_utils::data::grib2::cmc_glb(),
         (0, 0),
         f32::from_be_bytes([0x45, 0x0e, 0xcc, 0x05]),
         -2,
         1,
-        test_utils::data::flat_binary::cmc_glb_le()?
+        test_utils::data::flat_binary::cmc_glb_le()
     ),
     (
         decoding_complex_packing_with_num_descriptor_octet_being_3,
-        test_utils::data::grib2::noaa_gdas_0_10()?,
+        test_utils::data::grib2::noaa_gdas_0_10(),
         (1, 0),
         f32::from_be_bytes([0x00, 0x00, 0x00, 0x00]),
         1,
         8,
-        test_utils::data::flat_binary::noaa_gdas_1_le()?
+        test_utils::data::flat_binary::noaa_gdas_1_le()
     ),
     (
         decoding_complex_packing_with_3_byte_spatial_differencing_extra_descriptors_starting_from_0x80,
-        test_utils::data::grib2::noaa_gdas_0_10()?,
+        test_utils::data::grib2::noaa_gdas_0_10(),
         (0, 0),
         f32::from_be_bytes([0x49, 0x67, 0xe7, 0xdf]),
         1,
         1,
-        test_utils::data::flat_binary::noaa_gdas_0_le()?
+        test_utils::data::flat_binary::noaa_gdas_0_le()
     ),
     (
         decoding_complex_packing_with_zero_width_groups,
-        test_utils::data::grib2::noaa_gdas_0_10()?,
+        test_utils::data::grib2::noaa_gdas_0_10(),
         (2, 0),
         f32::from_be_bytes([0x00, 0x00, 0x00, 0x00]),
         3,
         9,
-        test_utils::data::flat_binary::noaa_gdas_2_le()?
+        test_utils::data::flat_binary::noaa_gdas_2_le()
     ),
     #[cfg(feature = "png-unpack-with-png-crate")]
     (
         decoding_png_packing_with_num_bits_being_24,
-        test_utils::data::grib2::noaa_mrms_merged_rho_hv()?,
+        test_utils::data::grib2::noaa_mrms_merged_rho_hv(),
         (0, 0),
         f32::from_be_bytes([0xc7, 0xc3, 0x1e, 0x00]),
         0,
         2,
-        test_utils::data::flat_binary::noaa_mrms_merged_rho_hv_le()?
+        test_utils::data::flat_binary::noaa_mrms_merged_rho_hv_le()
     ),
 }
--- src/test_utils/data.rs
+++ src/test_utils/data.rs
@@ -1,83 +1,59 @@
-use std::{
-    io,
-    path::{Path, PathBuf},
-};
-
-use crate::test_utils::decompress_to_vec;
-
-fn testdata_dir() -> PathBuf {
-    Path::new("testdata").into()
+macro_rules! definitions_of_test_data {
+    ($(($name:ident, $file_name:expr),)*) => ($(
+        #[allow(dead_code)]
+        pub(crate) fn $name() -> &'static str {
+            $file_name
+        }
+    )*);
 }
 
 pub(crate) mod grib2 {
-    use super::*;
-
-    macro_rules! definitions_of_grib2_test_data {
-        ($(($name:ident, $file_name:expr),)*) => ($(
-            #[allow(dead_code)]
-            pub(crate) fn $name() -> Result<Vec<u8>, io::Error> {
-                decompress_to_vec(testdata_dir().join($file_name))
-            }
-        )*);
-    }
-
-    definitions_of_grib2_test_data! {
-        (cmc_glb, "CMC_glb_TMP_ISBL_1_latlon.24x.24_2021051800_P000.grib2"),
-        (ecmwf_realtime_oper_fc_0, "20240101000000-0h-oper-fc.grib2.0-10.xz"),
-        (ecmwf_realtime_oper_fc_89, "20250912120000-0h-oper-fc.grib2.89.xz"),
+    definitions_of_test_data! {
+        (cmc_glb, "testdata/CMC_glb_TMP_ISBL_1_latlon.24x.24_2021051800_P000.grib2"),
+        (ecmwf_realtime_oper_fc_0, "testdata/20240101000000-0h-oper-fc.grib2.0-10.xz"),
+        (ecmwf_realtime_oper_fc_89, "testdata/20250912120000-0h-oper-fc.grib2.89.xz"),
         (
             jma_kousa,
-            "Z__C_RJTD_20170221120000_MSG_GPV_Gll0p5deg_Pys_B20170221120000_F2017022115-2017022212_grib2.bin.xz"
+            "testdata/Z__C_RJTD_20170221120000_MSG_GPV_Gll0p5deg_Pys_B20170221120000_F2017022115-2017022212_grib2.bin.xz"
         ),
-        (jma_meps, "Z__C_RJTD_20190605000000_MEPS_GPV_Rjp_L-pall_FH00-15_grib2.bin.0-20.xz"),
-        (jma_msmguid, "Z__C_RJTD_20190304000000_MSM_GUID_Rjp_P-all_FH03-39_Toorg_grib2.bin.xz"),
+        (jma_meps, "testdata/Z__C_RJTD_20190605000000_MEPS_GPV_Rjp_L-pall_FH00-15_grib2.bin.0-20.xz"),
+        (jma_msmguid, "testdata/Z__C_RJTD_20190304000000_MSM_GUID_Rjp_P-all_FH03-39_Toorg_grib2.bin.xz"),
         (
             jma_tornado_nowcast,
-            "Z__C_RJTD_20160822020000_NOWC_GPV_Ggis10km_Pphw10_FH0000-0100_grib2.bin"
+            "testdata/Z__C_RJTD_20160822020000_NOWC_GPV_Ggis10km_Pphw10_FH0000-0100_grib2.bin"
         ),
-        (ncmrwf_wind_solar, "wind_solar_ind_0.125_20240521_12Z.grib2.0.xz"),
-        (noaa_gdas_0_10, "gdas.t12z.pgrb2.0p25.f000.0-10.xz"),
-        (noaa_gdas_12, "gdas.t12z.pgrb2.0p25.f000.12.xz"),
-        (noaa_gdas_46, "gdas.t12z.pgrb2.0p25.f000.46.xz"),
-        (noaa_mrms_merged_rho_hv, "MRMS_MergedRhoHV_19.00_20260219-042039.grib2.gz"),
-        (noaa_mrms_precip_flag, "MRMS_PrecipFlag_00.00_20260219-042400.grib2.gz"),
-        (noaa_mrms_reflectivity, "MRMS_ReflectivityAtLowestAltitude_00.50_20230406-120039.grib2.gz"),
-        (noaa_ndfd_critfireo, "ds.critfireo.bin.xz"),
-        (noaa_ndfd_minrh, "ds.minrh.bin.xz"),
+        (ncmrwf_wind_solar, "testdata/wind_solar_ind_0.125_20240521_12Z.grib2.0.xz"),
+        (noaa_gdas_0_10, "testdata/gdas.t12z.pgrb2.0p25.f000.0-10.xz"),
+        (noaa_gdas_12, "testdata/gdas.t12z.pgrb2.0p25.f000.12.xz"),
+        (noaa_gdas_46, "testdata/gdas.t12z.pgrb2.0p25.f000.46.xz"),
+        (noaa_mrms_merged_rho_hv, "testdata/MRMS_MergedRhoHV_19.00_20260219-042039.grib2.gz"),
+        (noaa_mrms_precip_flag, "testdata/MRMS_PrecipFlag_00.00_20260219-042400.grib2.gz"),
+        (noaa_mrms_reflectivity, "testdata/MRMS_ReflectivityAtLowestAltitude_00.50_20230406-120039.grib2.gz"),
+        (noaa_ndfd_critfireo, "testdata/ds.critfireo.bin.xz"),
+        (noaa_ndfd_minrh, "testdata/ds.minrh.bin.xz"),
     }
 }
 
 pub(crate) mod flat_binary {
-    use super::*;
-
-    macro_rules! definitions_of_flat_binary_test_data {
-        ($(($name:ident, $file_name:expr),)*) => ($(
-            #[allow(dead_code)]
-            pub(crate) fn $name() -> Result<Vec<u8>, io::Error> {
-                decompress_to_vec(testdata_dir().join("gen").join($file_name))
-            }
-        )*);
-    }
-
-    definitions_of_flat_binary_test_data! {
-        (cmc_glb_le, "cmc-glb-wgrib2-le.bin.xz"),
-        (ecmwf_realtime_oper_fc_0_le, "ecmwf-realtime-oper-fc-0-le.bin.xz"),
-        (ecmwf_realtime_oper_fc_89_le, "ecmwf-realtime-oper-fc-89-le.bin.xz"),
-        (jma_kousa_le, "kousa-wgrib2-le.bin.xz"),
-        (jma_meps_le, "meps-wgrib2-le.bin.xz"),
-        (jma_msmguid_le, "msmguid-wgrib2-le.bin.xz"),
-        (jma_tornado_nowcast_le, "tornado-wgrib2-le.bin.xz"),
-        (ncmrwf_wind_solar_le, "wind_solar_ind_0.125_20240521_12Z.wgrib2-le.bin.xz"),
-        (noaa_gdas_0_le, "gdas-0-wgrib2-le.bin.xz"),
-        (noaa_gdas_1_le, "gdas-1-wgrib2-le.bin.xz"),
-        (noaa_gdas_2_le, "gdas-2-wgrib2-le.bin.xz"),
-        (noaa_gdas_12_le, "gdas-12-wgrib2-le.bin.xz"),
-        (noaa_gdas_46_le, "gdas-46-wgrib2-le.bin.xz"),
-        (noaa_mrms_merged_rho_hv_le, "mrms-merged-rho-hv-wgrib2-le.bin.xz"),
-        (noaa_mrms_precip_flag_le, "mrms-precip-flag-wgrib2-le.bin.xz"),
-        (noaa_mrms_reflectivity_le, "mrms-reflectivity-wgrib2-le.bin.xz"),
-        (noaa_ndfd_critfireo_0_le, "ds.critfireo.bin.0.xz"),
-        (noaa_ndfd_critfireo_1_le, "ds.critfireo.bin.1.xz"),
-        (noaa_ndfd_minrh_0_le, "ds.minrh.bin.0.xz"),
+    definitions_of_test_data! {
+        (cmc_glb_le, "testdata/gen/cmc-glb-wgrib2-le.bin.xz"),
+        (ecmwf_realtime_oper_fc_0_le, "testdata/gen/ecmwf-realtime-oper-fc-0-le.bin.xz"),
+        (ecmwf_realtime_oper_fc_89_le, "testdata/gen/ecmwf-realtime-oper-fc-89-le.bin.xz"),
+        (jma_kousa_le, "testdata/gen/kousa-wgrib2-le.bin.xz"),
+        (jma_meps_le, "testdata/gen/meps-wgrib2-le.bin.xz"),
+        (jma_msmguid_le, "testdata/gen/msmguid-wgrib2-le.bin.xz"),
+        (jma_tornado_nowcast_le, "testdata/gen/tornado-wgrib2-le.bin.xz"),
+        (ncmrwf_wind_solar_le, "testdata/gen/wind_solar_ind_0.125_20240521_12Z.wgrib2-le.bin.xz"),
+        (noaa_gdas_0_le, "testdata/gen/gdas-0-wgrib2-le.bin.xz"),
+        (noaa_gdas_1_le, "testdata/gen/gdas-1-wgrib2-le.bin.xz"),
+        (noaa_gdas_2_le, "testdata/gen/gdas-2-wgrib2-le.bin.xz"),
+        (noaa_gdas_12_le, "testdata/gen/gdas-12-wgrib2-le.bin.xz"),
+        (noaa_gdas_46_le, "testdata/gen/gdas-46-wgrib2-le.bin.xz"),
+        (noaa_mrms_merged_rho_hv_le, "testdata/gen/mrms-merged-rho-hv-wgrib2-le.bin.xz"),
+        (noaa_mrms_precip_flag_le, "testdata/gen/mrms-precip-flag-wgrib2-le.bin.xz"),
+        (noaa_mrms_reflectivity_le, "testdata/gen/mrms-reflectivity-wgrib2-le.bin.xz"),
+        (noaa_ndfd_critfireo_0_le, "testdata/gen/ds.critfireo.bin.0.xz"),
+        (noaa_ndfd_critfireo_1_le, "testdata/gen/ds.critfireo.bin.1.xz"),
+        (noaa_ndfd_minrh_0_le, "testdata/gen/ds.minrh.bin.0.xz"),
     }
 }
