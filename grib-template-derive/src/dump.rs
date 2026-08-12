0b0a81d252adb970cd9ff514cd201bebaa3cf6a9
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2026-08-12 22:37:47 +0900

    grib-template-derive: implement the attribute parser for doc overrides

--- grib-template-derive/src/dump.rs
+++ grib-template-derive/src/dump.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use quote::quote;
 
 pub(crate) fn impl_for_struct(
@@ -154,4 +156,77 @@ pub(crate) fn get_doc(attrs: &[syn::Attribute]) -> Option<String> {
         }
     }
     if doc.is_empty() { None } else { Some(doc) }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct DocOverrides(HashMap<String, String>);
+
+impl TryFrom<&syn::Attribute> for DocOverrides {
+    type Error = &'static str;
+
+    fn try_from(value: &syn::Attribute) -> Result<Self, Self::Error> {
+        const MESSAGE: &str = "error";
+        if !value.path().is_ident("dump") {
+            return Err(MESSAGE);
+        }
+        let meta = value.parse_args::<syn::Meta>().map_err(|_| MESSAGE)?;
+        if let syn::Meta::List(list) = meta {
+            if !list.path.is_ident("doc") {
+                return Err(MESSAGE);
+            }
+            let items = list
+                .parse_args_with(
+                    syn::punctuated::Punctuated::<syn::Meta, syn::Token![,]>::parse_terminated,
+                )
+                .map_err(|_| MESSAGE)?;
+
+            let mut map = HashMap::new();
+            for item in items {
+                if let syn::Meta::NameValue(nv) = item {
+                    let k = nv
+                        .path
+                        .get_ident()
+                        .map(|i| i.to_string())
+                        .unwrap_or_default();
+                    let v = if let syn::Expr::Lit(lit) = &nv.value
+                        && let syn::Lit::Str(s) = &lit.lit
+                    {
+                        s.value()
+                    } else {
+                        return Err(MESSAGE);
+                    };
+                    map.insert(k, v);
+                } else {
+                    return Err(MESSAGE);
+                }
+            }
+            Ok(Self(map))
+        } else {
+            Err(MESSAGE)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsing_doc_attr() -> Result<(), Box<dyn std::error::Error>> {
+        let attr: syn::Attribute = syn::parse_quote! {
+            #[dump(doc(
+                key1 = "val1",
+                key2 = "val2",
+            ))]
+        };
+        let actual = DocOverrides::try_from(&attr)?;
+        let expected = vec![("key1", "val1"), ("key2", "val2")]
+            .into_iter()
+            .map(|(k, v)| (k.to_owned(), v.to_owned()))
+            .collect::<HashMap<_, _>>();
+        let expected = DocOverrides(expected);
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
 }
