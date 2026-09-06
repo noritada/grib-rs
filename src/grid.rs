ea76bbd3670a6752619105212f584d71d56a5a0e
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2026-09-06 19:18:33 +0900

    Simplify the source of the test data byte sequence

--- src/grid.rs
+++ src/grid.rs
@@ -419,21 +419,12 @@ mod tests {
     use crate::def::grib2::template::param_set::{EarthShape, ScaledValue};
 
     #[test]
-    fn grid_definition_template_0() {
-        // data taken from submessage #0.0 of
-        // `Z__C_RJTD_20160822020000_NOWC_GPV_Ggis10km_Pphw10_FH0000-0100_grib2.
-        // bin.xz` in `testdata`
-        let data = GridDefinition::from_payload(
-            vec![
-                0x00, 0x00, 0x01, 0x50, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0xff, 0xff, 0xff, 0xff,
-                0xff, 0x01, 0x03, 0xcd, 0x39, 0xfa, 0x01, 0x03, 0xc9, 0xf6, 0xa3, 0x00, 0x00, 0x01,
-                0x00, 0x00, 0x00, 0x01, 0x50, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0x02,
-                0xdb, 0xc9, 0x3d, 0x07, 0x09, 0x7d, 0xa4, 0x30, 0x01, 0x31, 0xcf, 0xc3, 0x08, 0xef,
-                0xdd, 0x5c, 0x00, 0x01, 0xe8, 0x48, 0x00, 0x01, 0x45, 0x85, 0x00,
-            ]
-            .into_boxed_slice(),
-        )
-        .unwrap();
+    fn grid_definition_template_0() -> Result<(), Box<dyn std::error::Error>> {
+        let buf = crate::test_utils::decompress_to_vec(
+            crate::test_utils::data::grib2::JMA_TORNADO_NOWCAST,
+        )?;
+        let data =
+            GridDefinition::from_payload(buf[0x2a..0x6d].to_vec().into_boxed_slice()).unwrap();
 
         let actual = GridDefinitionTemplateValues::try_from(&data).unwrap();
         let expected = GridDefinitionTemplateValues::Template0(Template3_0 {
@@ -473,6 +464,7 @@ mod tests {
             },
         });
         assert_eq!(actual, expected);
+        Ok(())
     }
 }
 
